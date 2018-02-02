6dab1dee3370b4aa29a92600ac71496bfde4090f
Maik Macho <nuda1998@gmail.com>
2018-02-02 22:27:45 +0100

    More verbose RunWithLogging

---
+++ Celeste.Mod.mm/Patches/Monocle/Engine.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using Microsoft.Xna.Framework.Input;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Monocle {
+    class patch_Engine : Engine {
+
+        public patch_Engine(int width, int height, int windowWidth, int windowHeight, string windowTitle, bool fullscreen)
+            : base(width, height, windowWidth, windowHeight, windowTitle, fullscreen) {
+            // no-op. MonoMod ignores this - we only need this to make the compiler shut up.
+        }
+
+        [MonoModReplace]
+        public new void RunWithLogging() {
+            try {
+                base.Run();
+            } catch (Exception e) {
+                e.LogDetailed();
+                ErrorLog.Write(e);
+                ErrorLog.Open();
+            }
+        }
+
+    }
+}
