0a3474f49e2402f1deec8fd246aad2c935d21ced
Maik Macho <nuda1998@gmail.com>
2018-02-02 17:18:06 +0100

    Thanks, Visual Studio, for your broken .gitignore

--- Celeste.Mod.mm/Mod/EverestContentManager.cs
+++ Celeste.Mod.mm/Mod/EverestContentManager.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Xna.Framework.Content;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace Celeste.Mod {
     class EverestContentManager : ContentManager {
