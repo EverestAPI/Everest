403585eae39776056e8928484ee5b4dbe7d8de7a
leo60228 <iakornfeld@gmail.com>
2018-02-04 16:30:29 -0500

    Added MadelineEnergySelector

---
+++ MadelineExcitementSelector/MadelineEnergySelectorSettings.cs
@@ -0,0 +1,22 @@
+﻿using FMOD.Studio;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace Celeste.Mod.Energy {
+    [SettingName("MadelineEnergySelector")]
+    public class MadelineEnergySelectorSettings : EverestModuleSettings {
+
+        public bool Enabled { get; set; } = false;
+
+        [SettingRange(0, 15)]
+        public int Excitement { get; set; } = 1;
+
+    }
+}
