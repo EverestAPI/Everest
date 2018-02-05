c14235ab48206a4fc0df4f11f24a75dd52ff0ce7
LoLlYdE <11347524+LoLlYdE@users.noreply.github.com>
2018-02-05 20:39:13 +0100

    I dont even have any idea for another funny commit message

--- travis/html-gen.sh
+++ travis/html-gen.sh
@@ -17,14 +17,14 @@ function generateLink {
 	if [[ ${myarray[0]} != /everest-travis* ]] ; then
 	    return
 	fi
-	echo "<li><a href=\""${myarray[0]}"\">"${myarray[1]}"</a></li>" >> FILE
+	echo "<li><a href=\""${myarray[0]}"\">"${myarray[1]}"</a></li>" >> $FILE
 }
 
-preList >> FILE
+preList >> $FILE
 readarray array < builds_index.txt
 
 for (( idx=${#array[@]}-1 ; idx>=0 ; idx-- )) ; do
     generateLink "${array[idx]}"
 done
 
-postList >> FILE
+postList >> $FILE
