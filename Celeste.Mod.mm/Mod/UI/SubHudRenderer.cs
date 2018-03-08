783beed58bf81a9a877e69951cc82d76f9fa6f2d
Maik Macho <nuda1998@gmail.com>
2018-03-09 00:50:45 +0100

    Fix SubHudRenderer

--- Celeste.Mod.mm/Mod/UI/SubHudRenderer.cs
+++ Celeste.Mod.mm/Mod/UI/SubHudRenderer.cs
@@ -11,12 +11,25 @@ using System.Threading.Tasks;
 namespace Celeste.Mod.UI {
     public class SubHudRenderer : HiresRenderer {
 
+        public static bool Cleared;
+
+        public override void BeforeRender(Scene scene) {
+            base.BeforeRender(scene);
+            Cleared = true;
+        }
+
         public override void RenderContent(Scene scene) {
             if (!scene.Entities.HasVisibleEntities(TagsExt.SubHUD))
                 return;
             BeginRender(null, null);
             scene.Entities.RenderOnly(TagsExt.SubHUD);
             EndRender();
+        }
+
+        public override void Render(Scene scene) {
+            if (DrawToBuffer)
+                return; // Drawing to the HUD buffer.
+            base.Render(scene);
         }
 
     }
---
+++ Celeste.Mod.mm/Patches/HiresRenderer.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using Celeste.Mod.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Monocle;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Celeste {
+    class patch_HudRenderer : HudRenderer {
+
+        public override void BeforeRender(Scene scene) {
+            if (!DrawToBuffer)
+                return;
+
+            Engine.Graphics.GraphicsDevice.SetRenderTarget(Buffer);
+
+            if (!SubHudRenderer.Cleared) {
+                // SubHudRenderer.BeforeRender renders to the same buffer and clears it for us.
+                Engine.Graphics.GraphicsDevice.Clear(Color.Transparent);
+            }
+            SubHudRenderer.Cleared = false;
+
+            RenderContent(scene);
+        }
+
+    }
+}
