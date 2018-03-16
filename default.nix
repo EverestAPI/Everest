7731dcdb0fe3beb3ce5fe13aaf07518885d00648
leo60228 <iakornfeld@gmail.com>
2018-03-16 14:33:47 -0400

    Add nix package

---
+++ default.nix
@@ -0,0 +1,21 @@
+let nixpkgs = import <nixpkgs> {};
+    stdenv = nixpkgs.stdenv;
+
+in rec {
+  everest = nixpkgs.buildDotnetPackage rec {
+    baseName = "Everest";
+    version = "0.0.0";
+    name = "${baseName}-dev-${version}";
+    
+    src = ./.;
+
+    xBuildFiles = [ "Celeste.Mod.mm/Celeste.Mod.mm.csproj" "MiniInstaller/MiniInstaller.csproj" ];
+
+    outputFiles = [ "Celeste.Mod.mm/bin/Debug/*" /* vim syntax bug workaround */ "MiniInstaller/bin/Debug/*" /* vim syntax bug workaround */ ];
+    
+    postInstall = ''
+        mkdir -pv "$out/lib/dotnet/${baseName}"
+        sed -i '2i cd $1' $out/bin/miniinstaller
+    '';
+  };
+}
