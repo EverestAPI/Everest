42d2ad34da5e70bca83180d5fc41d1031501a0df
Maik Macho <nuda1998@gmail.com>
2018-05-02 17:49:28 +0200

    Return SFX handle as-is if no handle->eventname mapping found

---
+++ Celeste.Mod.mm/Patches/Sfxs.cs
@@ -0,0 +1,31 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using FMOD;
+using FMOD.Studio;
+using Microsoft.Xna.Framework.Input;
+using Monocle;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Celeste {
+    static class patch_Sfxs {
+
+        private static Dictionary<string, string> byHandle;
+
+        [MonoModReplace]
+        public static string EventnameByHandle(string handle) {
+            string result;
+            if (byHandle.TryGetValue(handle, out result))
+                return result;
+            return handle;
+        }
+
+    }
+}
