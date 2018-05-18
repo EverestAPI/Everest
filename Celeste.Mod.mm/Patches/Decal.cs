bf56d80a2cca04e1b209a0cc9af01cac5da592b2
Maik Macho <nuda1998@gmail.com>
2018-05-19 01:09:58 +0200

    Work around decal path bug in Maple / Ahorn

---
+++ Celeste.Mod.mm/Patches/Decal.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using Microsoft.Xna.Framework.Input;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Xna.Framework;
+using System.IO;
+
+namespace Celeste {
+    class patch_Decal : Decal {
+
+        public patch_Decal(string texture, Vector2 position, Vector2 scale, int depth)
+            : base(texture, position, scale, depth) {
+            // no-op. MonoMod ignores this - we only need this to make the compiler shut up.
+        }
+
+        public extern void orig_ctor_Decal(string texture, Vector2 position, Vector2 scale, int depth);
+        [MonoModConstructor]
+        public void ctor_Decal(string texture, Vector2 position, Vector2 scale, int depth) {
+            if (string.IsNullOrEmpty(Path.GetExtension(texture))) {
+                // Cruor temporarily broke decal paths in Maple / Ahorn.
+                texture += ".png";
+            }
+
+            orig_ctor_Decal(texture, position, scale, depth);
+        }
+
+    }
+}
