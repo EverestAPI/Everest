e8fd251714106fad3225d74473ae6dd2a018679d
Maik Macho <nuda1998@gmail.com>
2018-05-22 12:36:12 +0200

    Render "new" icon on title screen if an update is available

--- Celeste.Mod.mm/Patches/OuiTitleScreen.cs
+++ Celeste.Mod.mm/Patches/OuiTitleScreen.cs
@@ -2,6 +2,7 @@
 
 using Celeste.Mod;
 using Microsoft.Xna.Framework;
+using Monocle;
 using MonoMod;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,12 @@ namespace Celeste {
 
         // We're effectively in OuiTitleScreen, but still need to "expose" private fields to our mod.
         private string version;
+        private float textY;
+        private float alpha;
+
+        private MTexture updateTex;
+        private float updateAlpha;
+        private bool updateChecked;
 
         // Patching constructors is ugly.
         public extern void orig_ctor_OuiTitleScreen();
@@ -22,9 +29,30 @@ namespace Celeste {
             orig_ctor_OuiTitleScreen();
 
             version += $"\nEverest v.{Everest.Version}-{Everest.VersionTag}";
+            updateTex = GFX.Gui["areas/new"];
 
-            // Initialize DebugRC here, as the play mode can change.
+            // Initialize DebugRC here, as the play mode can change during the intro.
             Everest.DebugRC.Initialize();
+        }
+
+        public extern void orig_Update();
+        public override void Update() {
+            orig_Update();
+
+            if (!updateChecked && Everest.Updater.HasUpdate && alpha >= 1f) {
+                updateChecked = true;
+                Tween tween = Tween.Create(Tween.TweenMode.Oneshot, Ease.CubeInOut, 0.3f, true);
+                tween.OnUpdate = t => {
+                    updateAlpha = t.Percent;
+                };
+                Add(tween);
+            }
+        }
+
+        public extern void orig_Render();
+        public override void Render() {
+            orig_Render();
+            updateTex.DrawJustified(new Vector2(80f - 4f, textY + 8f * (1f - updateAlpha) + 2f), new Vector2(1f, 1f), Color.White * updateAlpha, 0.8f);
         }
 
     }
