4c412d4ae1c754fa38def6e8aa33afbb8293b2a2
Maik Macho <nuda1998@gmail.com>
2018-06-11 22:54:52 +0200

    Fix save data crash with leftover entries

--- Celeste.Mod.mm/Patches/SaveData.cs
+++ Celeste.Mod.mm/Patches/SaveData.cs
@@ -211,18 +211,22 @@ namespace Celeste {
                 // Refresh all stat IDs based on their SIDs, sort, fill and remove leftovers.
                 // Temporarily use ID_Unsafe; later ID_Safe to ID_Unsafe to resync the SIDs.
                 // This keeps the stats bound to their SIDs, not their indices, while removing non-existent areas.
+                int count = AreaData.Areas.Count(other => other.GetLevelSet() == set.Name);
+                // Fix IDs
                 for (int i = 0; i < areas.Count; i++)
                     ((patch_AreaStats) areas[i]).ID_Unsafe = AreaDataExt.Get(areas[i])?.ID ?? int.MaxValue;
+                // Sort
                 areas.Sort((a, b) => ((patch_AreaStats) a).ID_Unsafe - ((patch_AreaStats) b).ID_Unsafe);
+                // Remove leftovers
+                while (areas.Count > count)
+                    areas.RemoveAt(areas.Count - 1);
+                // Resync SIDs
                 for (int i = 0; i < areas.Count; i++)
                     ((patch_AreaStats) areas[i]).ID_Safe = ((patch_AreaStats) areas[i]).ID_Unsafe;
-
-                int count = AreaData.Areas.Count(other => other.GetLevelSet() == set.Name);
+                // Fill
                 for (int i = 0; i < count; i++)
                     if (i >= areas.Count || areas[i].ID != offset + i)
                         areas.Insert(i, new AreaStats(offset + i));
-                while (areas.Count > count)
-                    areas.RemoveAt(areas.Count - 1);
 
                 int lastCompleted = -1;
                 for (int i = 0; i < count; i++) {
