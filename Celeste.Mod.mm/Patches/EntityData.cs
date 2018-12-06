2a606863d78df650eec0bb9b2935e30e1b0725d0
Maik Macho <nuda1998@gmail.com>
2018-12-06 21:13:21 +0100

    Fix EntityData.Has not checking if Values is null

---
+++ Celeste.Mod.mm/Patches/EntityData.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using Celeste.Mod.Meta;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Monocle;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Celeste {
+    class patch_EntityData : EntityData {
+
+        public extern bool orig_Has(string key);
+        public new bool Has(string key) {
+            if (Values == null)
+                return false;
+            return orig_Has(key);
+        }
+
+    }
+}
