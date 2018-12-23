3c7d17e99a06b52e26ae9cfd3879ac71c57f80de
Maik Macho <nuda1998@gmail.com>
2018-12-23 13:32:47 +0100

    Add pre-build step to Azure Pipelines

---
+++ azure-pipelines-prebuild.ps1
@@ -0,0 +1,3 @@
+# Make the Everest version match the build number.
+$EverestPath = [io.path]::combine('Celeste.Mod.mm', 'Mod', 'Everest', 'Everest.cs')
+(Get-Content $EverestPath) -replace '(?<=\[public readonly static string VersionString = ")[^"]*', "1.$($env:BUILD_BUILDNUMBER).0-azure-$(($env:Build.SourceVersion).Substring(0, 5))" | Set-Content $EverestPath
