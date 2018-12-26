c11d34d98f18d2d4883fb8ea2bf1fdf523c1ea29
Maik Macho <nuda1998@gmail.com>
2018-12-26 13:18:08 +0100

    Fix whitelist param not dequeueing properly

--- Celeste.Mod.mm/Mod/Everest/Everest.cs
+++ Celeste.Mod.mm/Mod/Everest/Everest.cs
@@ -206,7 +206,7 @@ namespace Celeste.Mod {
                 else if (arg == "--everest-disabled" || arg == "--speedrun")
                     Environment.SetEnvironmentVariable("EVEREST_DISABLED", "1");
 
-                else if (arg == "--whitelist" && queue.Count > 1)
+                else if (arg == "--whitelist" && queue.Count >= 1)
                     Loader.NameWhitelist = queue.Dequeue();
 
             }
