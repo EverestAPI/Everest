8fa054828b9d4344f8ea2d4d7bc6aed298b6829b
Maik Macho <nuda1998@gmail.com>
2019-03-06 22:05:17 +0100

    Patch "UnlockEverythingThingy" to affect all levelsets (Thanks, orb!)

---
+++ Celeste.Mod.mm/Patches/UnlockEverythingThingy.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using Microsoft.Xna.Framework.Input;
+using Monocle;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Xna.Framework;
+
+namespace Celeste {
+    class patch_UnlockEverythingThingy : UnlockEverythingThingy {
+
+        [MonoModReplace]
+        public new void UnlockEverything(Level level) {
+            patch_SaveData data = (patch_SaveData) SaveData.Instance;
+            foreach (LevelSetStats set in data.LevelSets) {
+                set.UnlockedAreas = SaveData.Instance.MaxArea;
+            }
+            data.CheatMode = true;
+
+            Settings.Instance.Pico8OnMainMenu = true;
+            Settings.Instance.VariantsUnlocked = true;
+
+            level.Session.InArea = false;
+
+            Engine.Scene = new LevelExit(LevelExit.Mode.GiveUp, level.Session);
+        }
+
+    }
+}
