76ff49584a5d6828c4058a14729386fd60d1d4cf
DemoJameson <demojameson@gmail.com>
2019-03-14 13:18:26 +0800

    Fixed an issue with vanilla itself, player's body trail is always facing right.

---
+++ Celeste.Mod.mm/Patches/TrailManager.cs
@@ -0,0 +1,21 @@
+#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste {
+    public class patch_TrailManager : TrailManager {
+        public class patch_Snapshot : Snapshot {
+            public extern void orig_Init(TrailManager manager, int index, Vector2 position, Image sprite, PlayerHair hair, Color color, float duration, int depth);
+
+            public new void Init(TrailManager manager, int index, Vector2 position, Image sprite, PlayerHair hair, Color color, float duration, int depth) {
+                orig_Init(manager, index, position, sprite, hair, color, duration, depth);
+
+                // Fixed an issue with vanilla itself, player's body trail is always facing right 
+                if (sprite != null && hair != null) {
+                    SpriteScale.X = SpriteScale.Abs().X * (int) hair.Facing;
+                }
+            }
+        }
+    }
+}
