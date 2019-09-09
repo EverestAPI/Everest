25e15386bd08a58864a459e4c7a72d315b5600d5
max480 <max480@free.fr>
2019-09-09 21:05:56 +0200

    Code style fixes

--- Celeste.Mod.mm/Patches/TrailManager.cs
+++ Celeste.Mod.mm/Patches/TrailManager.cs
@@ -1,14 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Monocle;
 
-namespace Celeste
-{
+namespace Celeste {
     class patch_TrailManager : TrailManager {
 
         // Make this signature accessible to older mods.
         public static void Add(Entity entity, Color color, float duration = 1f) {
-            TrailManager.Add(entity, color, duration);
+            TrailManager.Add(entity, color, duration, false, false);
         }
-
     }
 }
