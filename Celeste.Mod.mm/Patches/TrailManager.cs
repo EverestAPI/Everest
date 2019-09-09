72c4edebf19048b89fc5e54a9bb8c0d2f728d4ae
max480 <max480@free.fr>
2019-09-09 20:59:17 +0200

    Cleanup for the previous commit

--- Celeste.Mod.mm/Patches/TrailManager.cs
+++ Celeste.Mod.mm/Patches/TrailManager.cs
@@ -1,18 +1,8 @@
-﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
-
-using Celeste.Mod;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
+﻿using Microsoft.Xna.Framework;
 using Monocle;
-using MonoMod;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 
-namespace Celeste {
+namespace Celeste
+{
     class patch_TrailManager : TrailManager {
 
         // Make this signature accessible to older mods.
