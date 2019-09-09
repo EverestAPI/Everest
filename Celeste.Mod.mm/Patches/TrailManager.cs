5b64186acdefe6abfa217f19169e4cf0f9269dbb
max480 <max480@free.fr>
2019-09-09 20:56:55 +0200

    Added shim for TrailManager to allow GhostNet to work

---
+++ Celeste.Mod.mm/Patches/TrailManager.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Monocle;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Celeste {
+    class patch_TrailManager : TrailManager {
+
+        // Make this signature accessible to older mods.
+        public static void Add(Entity entity, Color color, float duration = 1f) {
+            TrailManager.Add(entity, color, duration);
+        }
+
+    }
+}
