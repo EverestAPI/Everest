ab838cfa837ae645e2141a9465690d855a6c8b91
max480 <max480@free.fr>
2019-09-09 23:11:25 +0200

    Shim the Dust.Burst(Vector2, float, int) method

---
+++ Celeste.Mod.mm/Patches/Dust.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Celeste {
+    // Dust is static.
+    class patch_Dust {
+        public static extern void orig_Initialize();
+
+        // Make this signature accessible to older mods.
+        public static void Burst(Vector2 position, float direction, int count = 1) {
+            Dust.Burst(position, direction, count, null);
+        }
+    }
+}
