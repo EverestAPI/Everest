dc8e5e4d9ffcbc2224cb00044096e2b0661a5181
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-10-19 18:10:21 +0200

    ActivateDreamBlocksTrigger. Activates Dream Blocks for any inventory with animation.

---
+++ Celeste.Mod.mm/Mod/Entities/ActivateDreamBlocksTrigger.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.Entities
+{
+    [CustomEntity("everest/activateDreamBlocksTrigger")]
+    class ActivateDreamBlocksTrigger : Trigger
+    {
+        private readonly Level level;
+
+        public ActivateDreamBlocksTrigger(EntityData data, Vector2 offset)
+            : base(data, offset)
+        {
+            level = Scene as Level;
+        }
+
+        public override void OnEnter(Player player)
+        {
+            if (!level.Session.Inventory.DreamDash)
+            {
+                level.Session.Inventory.DreamDash = true;
+                foreach (DreamBlock dreamBlock in level.Tracker.GetEntities<DreamBlock>())
+                {
+                    dreamBlock.Activate();
+                }
+            }
+        }
+    }
+}
