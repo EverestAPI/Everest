bbc97dceb8d6638cff5b53e9548b9b98c533accb
Maik Macho <nuda1998@gmail.com>
2019-12-04 15:00:54 +0100

    Attempt to provide old SlashFx::Burst signature

---
+++ Celeste.Mod.mm/Patches/SlashFx.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using Celeste.Mod;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod;
+using MonoMod.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste {
+    class patch_SlashFx : SlashFx {
+
+        [MonoModLinkFrom("System.Void Celeste.SlashFx::Burst(Microsoft.Xna.Framework.Vector2,System.Single)")]
+        public static void _Burst(Vector2 position, float direction) {
+            Burst(position, direction);
+        }
+
+    }
+}
