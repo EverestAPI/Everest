909a6e13fd36a521cf958d73afdb291dcaa5b6d8
max480 <max480@free.fr>
2019-12-06 19:48:07 +0100

    Fix the mod updates feature (following a server-side update)

--- Celeste.Mod.mm/Mod/Helpers/ModUpdateInfo.cs
+++ Celeste.Mod.mm/Mod/Helpers/ModUpdateInfo.cs
@@ -7,5 +7,7 @@ namespace Celeste.Mod.Helpers {
         public virtual int LastUpdate { get; set; }
         public virtual string URL { get; set; }
         public virtual List<string> xxHash { get; set; }
+        public virtual string GameBananaType { get; set; }
+        public virtual int GameBananaId { get; set; }
     }
 }
