f824b340871af69c4c22c373967ed789ab80434f
Maik Macho <nuda1998@gmail.com>
2020-01-09 15:02:00 +0100

    Heavily revamp how map data gets processed / fixed before loading

---
+++ Celeste.Mod.mm/Mod/Helpers/MapDataFixup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Celeste.Mod.Core;
+using Celeste.Mod.Meta;
+
+namespace Celeste.Mod {
+    public class MapDataFixup {
+
+        public List<EverestMapDataProcessor> Processors = new List<EverestMapDataProcessor>();
+        public readonly MapData MapData;
+        public readonly AreaKey AreaKey;
+        public readonly AreaData AreaData;
+        public readonly ModeProperties Mode;
+        public BinaryPacker.Element Root;
+
+        public MapDataFixup(MapData map) {
+            MapData = map;
+            AreaKey = map.Area;
+            AreaData = AreaData.Get(AreaKey);
+            Mode = AreaData.Mode[(int) AreaKey.Mode];
+
+            foreach (EverestModule module in Everest._Modules) {
+                module.PrepareMapDataProcessors(this);
+            }
+        }
+
+        public void Add<T>() where T : EverestMapDataProcessor, new() {
+            Add(new T());
+        }
+
+        public void Add<T>(T p) where T : EverestMapDataProcessor {
+            p._Init(this);
+            Processors.Add(p);
+        }
+
+        public T Get<T>() where T : EverestMapDataProcessor {
+            foreach (EverestMapDataProcessor p in Processors)
+                if (p is T)
+                    return (T) p;
+            return null;
+        }
+
+        public void Process(BinaryPacker.Element root) {
+            Root = root;
+
+            foreach (EverestMapDataProcessor p in Processors)
+                p.Reset();
+
+            Run("root", root);
+
+            foreach (EverestMapDataProcessor p in Processors)
+                p.End();
+        }
+
+        public void Run(string stepName, BinaryPacker.Element el) {
+            foreach (EverestMapDataProcessor p in Processors) {
+                if (p.Steps.TryGetValue(stepName, out Action<BinaryPacker.Element> step))
+                    step(el);
+            }
+        }
+
+    }
+}
---
+++ Celeste.Mod.mm/Mod/Module/EverestMapDataProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod {
+    public abstract class EverestMapDataProcessor {
+
+        public MapDataFixup Context { get; private set; }
+        public AreaKey AreaKey => Context.AreaKey;
+        public AreaData AreaData => Context.AreaData;
+        public ModeProperties Mode => Context.Mode;
+        public Dictionary<string, Action<BinaryPacker.Element>> Steps { get; private set; }
+
+        public abstract void Reset();
+
+        internal void _Init(MapDataFixup context) {
+            Context = context;
+            Steps = Init();
+        }
+        public abstract Dictionary<string, Action<BinaryPacker.Element>> Init();
+
+        public abstract void End();
+
+    }
+}
