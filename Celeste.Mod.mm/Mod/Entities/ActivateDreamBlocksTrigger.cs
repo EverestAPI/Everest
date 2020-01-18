d9acb5fbe8dcb80bd61a02379f1755870b7d1af3
Natalie Greyfin <mariomister541@gmail.com>
2020-01-17 16:53:38 -0800

    Really fixed ActivateDreamBlocksTrigger this time.
    Honest! Even tested it and everything.

--- Celeste.Mod.mm/Mod/Entities/ActivateDreamBlocksTrigger.cs
+++ Celeste.Mod.mm/Mod/Entities/ActivateDreamBlocksTrigger.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.Xna.Framework;
+using Monocle;
 
 namespace Celeste.Mod.Entities
 {
     [CustomEntity("everest/activateDreamBlocksTrigger")]
     class ActivateDreamBlocksTrigger : Trigger
     {
+        private bool rumble;
         public ActivateDreamBlocksTrigger(EntityData data, Vector2 offset)
-            : base(data, offset) { }
+            : base(data, offset)
+        {
+            rumble = data.Bool("fullRoutine");
+        }
 
         public override void OnEnter(Player player)
         {
@@ -16,7 +21,10 @@ namespace Celeste.Mod.Entities
                 level.Session.Inventory.DreamDash = true;
                 foreach (DreamBlock dreamBlock in level.Tracker.GetEntities<DreamBlock>())
                 {
-                    dreamBlock.Activate();
+                    if (rumble)
+                        dreamBlock.Add(new Coroutine(dreamBlock.Activate(), true));
+                    else
+                        dreamBlock.ActivateNoRoutine();
                 }
             }
         }
