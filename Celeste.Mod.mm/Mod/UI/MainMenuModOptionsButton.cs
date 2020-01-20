0a07b2e7f22a1856653e0f183f998079f8ed7849
max480 <max480@free.fr>
2020-01-20 19:43:48 +0100

    Display an error message on the main menu if a mod failed to load

---
+++ Celeste.Mod.mm/Mod/UI/MainMenuModOptionsButton.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.UI {
+    class MainMenuModOptionsButton : MainMenuSmallButton {
+        private string subText;
+
+        public override float ButtonHeight {
+            get {
+                if (subText == null)
+                    return ActiveFont.LineHeight * 1.25f;
+
+                return ActiveFont.LineHeight * 1.75f;
+            }
+        }
+
+        public MainMenuModOptionsButton(string labelName, string iconName, Oui oui, Vector2 targetPosition, Vector2 tweenFrom, Action onConfirm)
+            : base(labelName, iconName, oui, targetPosition, tweenFrom, onConfirm) {
+
+            int delayedModCount = Everest.Loader.Delayed.Count;
+            if (delayedModCount > 1) {
+                subText = string.Format(Dialog.Get("MENU_MODOPTIONS_MULTIPLE_MODS_FAILEDTOLOAD"), delayedModCount);
+            } else if (delayedModCount == 1) {
+                subText = Dialog.Clean("MENU_MODOPTIONS_ONE_MOD_FAILEDTOLOAD");
+            } else {
+                subText = null;
+            }
+        }
+
+        public override void Render() {
+            base.Render();
+
+            if (subText != null) {
+                Vector2 offset = new Vector2(Ease.CubeInOut(this.GetEase()) * 32f, this.GetWiggler().Value * 8f);
+                ActiveFont.DrawOutline(subText, Position + offset + new Vector2(84f, 84f), new Vector2(0f, 0.5f), Vector2.One * 0.6f, Color.OrangeRed, 2f, Color.Black);
+            }
+        }
+    }
+}
