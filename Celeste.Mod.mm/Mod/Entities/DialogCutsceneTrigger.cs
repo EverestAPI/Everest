444ca318055d17dde955a48359cf0f8e8bb2b710
max480 <max480@free.fr>
2020-02-07 21:10:26 +0100

    Add deathCount attribute to Dialog Cutscene Trigger
    Works in the same way as for Minitextbox Trigger: number of deaths to reach in the room for it to activate, any negative number to disable.

--- Celeste.Mod.mm/Mod/Entities/DialogCutsceneTrigger.cs
+++ Celeste.Mod.mm/Mod/Entities/DialogCutsceneTrigger.cs
@@ -9,19 +9,25 @@ namespace Celeste.Mod.Entities {
         private EntityID id;
         private bool onlyOnce;
         private bool endLevel;
+        private int deathCount;
 
         public DialogCutsceneTrigger(EntityData data, Vector2 offset, EntityID entId)
             : base(data, offset) {
             dialogEntry = data.Attr("dialogId");
             onlyOnce = data.Bool("onlyOnce", true);
             endLevel = data.Bool("endLevel", false);
+            deathCount = data.Int("deathCount", -1);
             triggered = false;
             id = entId;
         }
 
         public override void OnEnter(Player player) {
-            if (triggered || (Scene as Level).Session.GetFlag("DoNotLoad" + id))
+            if (triggered || (Scene as Level).Session.GetFlag("DoNotLoad" + id) ||
+                (deathCount >= 0 && SceneAs<Level>().Session.DeathsInCurrentLevel != deathCount)) {
+
                 return;
+            }
+
             triggered = true;
 
             Scene.Add(new DialogCutscene(dialogEntry, player, endLevel));
