f6256df83172d3184d80824caaf4d360067b0e26
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2020-04-10 00:59:14 -0700

    Added OptionSubMenu for multiple-choice submenus

--- Celeste.Mod.mm/Mod/UI/TextMenuExt.cs
+++ Celeste.Mod.mm/Mod/UI/TextMenuExt.cs
@@ -775,5 +775,419 @@ namespace Celeste {
             }
             #endregion
         }
+        /// <summary>
+		/// <see cref="TextMenu.Item"/> that acts as a Slider of Submenus for other Items.
+		/// <br></br><br></br>
+		/// Currently does not support recursive submenus
+		/// </summary>
+		public class OptionSubMenu : TextMenu.Item {
+            public string Label;
+            MTexture Icon;
+
+            public List<Tuple<string, List<TextMenu.Item>>> Menus { get; private set; }
+
+            private List<Tuple<string, List<TextMenu.Item>>> delayedAddMenus;
+
+            public int MenuIndex;
+            private int InitialSelection;
+            public int Selection;
+            private int lastDir;
+            private float sine;
+            public Action<int> OnValueChange;
+
+            public List<TextMenu.Item> CurrentMenu {
+                get { return (Menus.Count > 0) ? Menus[MenuIndex].Item2 : null; }
+            }
+            public TextMenu.Item Current {
+                get {
+                    if (CurrentMenu.Count <= 0 || Selection < 0) {
+                        return null;
+                    }
+                    return CurrentMenu[Selection];
+                }
+            }
+            public int FirstPossibleSelection {
+                get {
+                    for (int i = 0; i < CurrentMenu.Count; i++) {
+                        if (CurrentMenu[i] != null && CurrentMenu[i].Hoverable) {
+                            return i;
+                        }
+                    }
+                    return 0;
+                }
+            }
+            public int LastPossibleSelection {
+                get {
+                    for (int i = CurrentMenu.Count - 1; i >= 0; i--) {
+                        if (CurrentMenu[i] != null && CurrentMenu[i].Hoverable) {
+                            return i;
+                        }
+                    }
+                    return 0;
+                }
+            }
+            public float ScrollTargetY {
+                get {
+                    float min = Engine.Height - 150 - Container.Height * Container.Justify.Y;
+                    float max = 150f + Container.Height * Container.Justify.Y;
+                    return Calc.Clamp(Engine.Height / 2 + Container.Height * Container.Justify.Y - this.GetYOffsetOf(Current), min, max);
+                }
+            }
+
+            public float ItemSpacing;
+            public float ItemIndent;
+            private Color HighlightColor;
+            public string ConfirmSfx;
+            public bool AlwaysCenter;
+
+            public float LeftColumnWidth;
+            public float RightColumnWidth;
+
+            public float TitleHeight { get; private set; }
+            public float MenuHeight { get; private set; }
+            private float _MenuHeight;
+
+            public bool Focused;
+            private bool wasFocused;
+
+            private bool containerAutoScroll;
+
+            public OptionSubMenu(string label) : base() {
+                //Item Constructor
+                ConfirmSfx = "event:/ui/main/button_select";
+                Label = label;
+                Icon = GFX.Gui["downarrow"];
+                Selectable = true;
+                IncludeWidthInMeasurement = true;
+                MenuIndex = 0;
+
+
+                //Menu Constructor
+                Menus = new List<Tuple<string, List<TextMenu.Item>>>();
+                delayedAddMenus = new List<Tuple<string, List<TextMenu.Item>>>();
+                Selection = -1;
+                ItemSpacing = 4f;
+                ItemIndent = 20f;
+                HighlightColor = Color.White;
+
+                RecalculateSize();
+            }
+            #region Menu
+            /// <summary>
+            /// Add a list of non-submenu <see cref="TextMenu.Item"/>s to the Submenu
+            /// </summary>
+            /// <param name="label">Displayed submenu label</param>
+            /// <param name="items">Items to be added to the submenu</param>
+            /// <returns></returns>
+            public OptionSubMenu Add(string label, List<TextMenu.Item> items) {
+                if (Container != null) {
+                    orig_Add(label, items);
+                    return this;
+                } else {
+                    delayedAddMenus.Add(new Tuple<string, List<TextMenu.Item>>(label, items));
+                    return this;
+                }
+            }
+            public OptionSubMenu orig_Add(string label, List<TextMenu.Item> items) {
+                if (items != null) {
+                    foreach (TextMenu.Item item in items) {
+                        item.Container = this.Container;
+                        Container.Add(item.ValueWiggler = Wiggler.Create(0.25f, 3f, null, false, false));
+                        Container.Add(item.SelectWiggler = Wiggler.Create(0.25f, 3f, null, false, false));
+                        item.ValueWiggler.UseRawDeltaTime = (item.SelectWiggler.UseRawDeltaTime = true);
+                        item.Added();
+                    }
+                    Menus.Add(new Tuple<string, List<TextMenu.Item>>(label, items));
+                } else
+                    Menus.Add(new Tuple<string, List<TextMenu.Item>>(label, new List<TextMenu.Item>()));
+                if (Selection == -1) {
+                    FirstSelection();
+                }
+                RecalculateSize();
+                return this;
+            }
+
+            public OptionSubMenu SetInitialSelection(int index) {
+                InitialSelection = index;
+                return this;
+            }
+            public void Clear() {
+                Menus = new List<Tuple<string, List<TextMenu.Item>>>();
+            }
+            public void FirstSelection() {
+                Selection = -1;
+                if (CurrentMenu.Count > 0)
+                    MoveSelection(1, true);
+            }
+            public void MoveSelection(int direction, bool wiggle = false) {
+                int selection = Selection;
+                direction = Math.Sign(direction);
+                int num = 0;
+                using (List<TextMenu.Item>.Enumerator enumerator = CurrentMenu.GetEnumerator()) {
+                    while (enumerator.MoveNext()) {
+                        if (enumerator.Current.Hoverable) {
+                            num++;
+                        }
+                    }
+                }
+                bool flag = num > 2;
+                do {
+                    Selection += direction;
+                    if (flag) {
+                        if (Selection < 0) {
+                            Selection = CurrentMenu.Count - 1;
+                        } else if (Selection >= CurrentMenu.Count) {
+                            Selection = 0;
+                        }
+                    } else if (Selection < 0 || Selection > CurrentMenu.Count - 1) {
+                        Selection = Calc.Clamp(Selection, 0, CurrentMenu.Count - 1);
+                        break;
+                    }
+                }
+                while (!Current.Hoverable);
+                if (!Current.Hoverable) {
+                    Selection = selection;
+                }
+                if (Selection != selection && Current != null) {
+                    if (selection >= 0 && CurrentMenu[selection] != null && CurrentMenu[selection].OnLeave != null) {
+                        CurrentMenu[selection].OnLeave();
+                    }
+                    Current.OnEnter?.Invoke();
+                    if (wiggle) {
+                        Audio.Play((direction > 0) ? "event:/ui/main/rollover_down" : "event:/ui/main/rollover_up");
+                        Current.SelectWiggler.Start();
+                    }
+                }
+            }
+            public void RecalculateSize() {
+                TitleHeight = ActiveFont.LineHeight;
+
+                LeftColumnWidth = (RightColumnWidth = (_MenuHeight = 0f));
+                if (Menus.Count < 1 || CurrentMenu == null)
+                    return;
+
+                foreach (TextMenu.Item item in CurrentMenu) {
+                    if (item.IncludeWidthInMeasurement) {
+                        LeftColumnWidth = Math.Max(LeftColumnWidth, item.LeftWidth());
+                    }
+                }
+                foreach (TextMenu.Item item2 in CurrentMenu) {
+                    if (item2.IncludeWidthInMeasurement) {
+                        RightColumnWidth = Math.Max(RightColumnWidth, item2.RightWidth());
+                    }
+                }
+                foreach (TextMenu.Item item3 in CurrentMenu) {
+                    if (item3.Visible) {
+                        _MenuHeight += item3.Height() + Container.ItemSpacing;
+                    }
+                }
+                _MenuHeight -= Container.ItemSpacing;
+            }
+            public float GetYOffsetOf(TextMenu.Item item) {
+                if (item == null) {
+                    return 0f;
+                }
+                float offset = 0f;
+                foreach (TextMenu.Item item2 in CurrentMenu) {
+                    if (item2.Visible) {
+                        offset += item2.Height() + ItemSpacing;
+                    }
+                    if (item2 == item) {
+                        break;
+                    }
+                }
+                return offset - item.Height() * 0.5f - ItemSpacing + Container.GetYOffsetOf(this);
+            }
+
+            private void menu_Added() {
+                foreach (Tuple<string, List<TextMenu.Item>> menu in delayedAddMenus) {
+                    orig_Add(menu.Item1, menu.Item2);
+                }
+                MenuIndex = InitialSelection;
+            }
+            private void menu_Update() {
+                OnUpdate?.Invoke();
+                //ease check needed to eat the first input from this.Container
+                if (Focused) {
+                    if (!wasFocused) {
+                        wasFocused = true;
+                        goto AfterInput;
+                    }
+                    if (Input.MenuDown.Pressed) {
+                        if (!Input.MenuDown.Repeating || Selection != LastPossibleSelection) {
+                            MoveSelection(1, true);
+                        }
+                    } else if (Input.MenuUp.Pressed && (!Input.MenuUp.Repeating || Selection != FirstPossibleSelection)) {
+                        MoveSelection(-1, true);
+                    }
+                    if (Current != null) {
+                        if (Input.MenuLeft.Pressed) {
+                            Current.LeftPressed();
+                        }
+                        if (Input.MenuRight.Pressed) {
+                            Current.RightPressed();
+                        }
+                        if (Input.MenuConfirm.Pressed) {
+                            Current.ConfirmPressed();
+                            Current.OnPressed?.Invoke();
+                        }
+                        if (Input.MenuJournal.Pressed && Current.OnAltPressed != null) {
+                            Current.OnAltPressed();
+                        }
+                    }
+                    if (!Input.MenuConfirm.Pressed) {
+                        if (Input.MenuCancel.Pressed || Input.ESC.Pressed || Input.Pause.Pressed) {
+                            this.Focused = false;
+                            Audio.Play("event:/ui/main/button_back");
+                            Container.AutoScroll = containerAutoScroll;
+                            Container.Focused = true;
+                        }
+                    }
+                } else
+                    wasFocused = false;
+                AfterInput:
+                foreach (TextMenu.Item item in CurrentMenu) {
+                    item.OnUpdate?.Invoke();
+                    item.Update();
+                }
+                if (Settings.Instance.DisableFlashes) {
+                    HighlightColor = TextMenu.HighlightColorA;
+                } else if (Engine.Scene.OnRawInterval(0.1f)) {
+                    if (HighlightColor == TextMenu.HighlightColorA) {
+                        HighlightColor = TextMenu.HighlightColorB;
+                    } else {
+                        HighlightColor = TextMenu.HighlightColorA;
+                    }
+                }
+                if (containerAutoScroll) {
+                    if (Container.Height > Container.ScrollableMinSize) {
+                        Container.Position.Y += (this.ScrollTargetY - Container.Position.Y) * (1f - (float) Math.Pow(0.0099999997764825821, (double) Engine.RawDeltaTime));
+                        return;
+                    }
+                    Container.Position.Y = 540f;
+                }
+            }
+            private void menu_Render(Vector2 position) {
+                RecalculateSize();
+                foreach (TextMenu.Item item in CurrentMenu) {
+                    if (item.Visible) {
+                        float num = item.Height();
+                        Vector2 vector = position + new Vector2(0f, num * 0.5f + item.SelectWiggler.Value * 8f);
+                        if (vector.Y + num * 0.5f > 0f && vector.Y - num * 0.5f < (float) Engine.Height) {
+                            item.Render(vector, Focused && Current == item);
+                        }
+                        position.Y += num + ItemSpacing;
+                    }
+                }
+            }
+            #endregion
+            #region TextMenu.Item
+
+            public OptionSubMenu Change(Action<int> onValueChange) {
+                this.OnValueChange = onValueChange;
+                return this;
+            }
+            public override void LeftPressed() {
+                if (MenuIndex > 0) {
+                    Audio.Play("event:/ui/main/button_toggle_off");
+                    MenuIndex--;
+                    lastDir = -1;
+                    ValueWiggler.Start();
+                    FirstSelection();
+                    OnValueChange?.Invoke(MenuIndex);
+                }
+            }
+            public override void RightPressed() {
+                if (MenuIndex < Menus.Count - 1) {
+                    Audio.Play("event:/ui/main/button_toggle_on");
+                    MenuIndex++;
+                    lastDir = 1;
+                    ValueWiggler.Start();
+                    FirstSelection();
+                    OnValueChange?.Invoke(MenuIndex);
+                }
+            }
+            public override void ConfirmPressed() {
+                if (CurrentMenu.Count > 0) {
+                    containerAutoScroll = Container.AutoScroll;
+                    Container.AutoScroll = false;
+                    Container.Focused = false;
+                    this.Focused = true;
+                    FirstSelection();
+                }
+            }
+            public override float LeftWidth() {
+                return ActiveFont.Measure(Label).X;
+            }
+            public override float RightWidth() {
+                float num = 0f;
+                foreach (string menu in Menus.Select(tuple => tuple.Item1)) {
+                    num = Math.Max(num, ActiveFont.Measure(menu).X);
+                }
+                return num + 120f;
+            }
+            public override float Height() {
+                //If there are no items, MenuHeight will actually be a negative number
+                if (CurrentMenu != null && CurrentMenu.Count > 0)
+                    return TitleHeight + (MenuHeight);
+                else
+                    return TitleHeight;
+            }
+            public override void Added() {
+                base.Added();
+                menu_Added();
+            }
+            public override void Update() {
+                MenuHeight = Calc.Approach(MenuHeight, _MenuHeight, Engine.DeltaTime * Math.Abs(MenuHeight - _MenuHeight) * 8f);
+
+                sine += Engine.RawDeltaTime;
+                base.Update();
+                if (CurrentMenu != null)
+                    menu_Update();
+            }
+            public override void Render(Vector2 position, bool highlighted) {
+                Vector2 top = new Vector2(position.X, position.Y - (Height() / 2));
+
+                float alpha = Container.Alpha;
+                Color color = Disabled ? Color.DarkSlateGray : ((highlighted ? Container.HighlightColor : Color.White) * alpha);
+                Color strokeColor = Color.Black * (alpha * alpha * alpha);
+
+                bool flag = Container.InnerContent == TextMenu.InnerContentMode.TwoColumn && !this.AlwaysCenter;
+
+                Vector2 titlePosition = top + (Vector2.UnitY * this.TitleHeight / 2) + (flag ? Vector2.Zero : new Vector2(this.Container.Width * 0.5f, 0f));
+                Vector2 justify = (flag) ? new Vector2(0f, 0.5f) : new Vector2(0.5f, 0.5f);
+                Vector2 iconJustify = (flag) ? new Vector2(ActiveFont.Measure(Label).X + Icon.Width, 5f) : new Vector2(ActiveFont.Measure(Label).X / 2 + Icon.Width, 5f);
+                OptionSubMenu.DrawIcon(titlePosition, Icon, iconJustify, true, (Disabled || CurrentMenu?.Count < 1 ? Color.DarkSlateGray : (this.Focused ? Container.HighlightColor : Color.White)) * alpha, 0.8f);
+                ActiveFont.DrawOutline(Label, titlePosition, justify, Vector2.One, color, 2f, strokeColor);
+                if (this.Menus.Count > 0) {
+                    float rWidth = RightWidth();
+
+                    ActiveFont.DrawOutline(Menus[MenuIndex].Item1, titlePosition + new Vector2(Container.Width - rWidth * 0.5f + lastDir * ValueWiggler.Value * 8f, 0f), new Vector2(0.5f, 0.5f), Vector2.One * 0.8f, color, 2f, strokeColor);
+                    Vector2 vector = Vector2.UnitX * (highlighted ? ((float) Math.Sin(sine * 4f) * 4f) : 0f);
+                    Color color2 = MenuIndex > 0 ? color : (Color.DarkSlateGray * alpha);
+                    Vector2 position2 = titlePosition + new Vector2(Container.Width - rWidth + 40f + ((lastDir < 0) ? (-ValueWiggler.Value * 8f) : 0f), 0f) - (MenuIndex > 0 ? vector : Vector2.Zero);
+                    ActiveFont.DrawOutline("<", position2, new Vector2(0.5f, 0.5f), Vector2.One, color2, 2f, strokeColor);
+
+                    bool flag2 = MenuIndex < Menus.Count - 1;
+                    Color color3 = flag2 ? color : (Color.DarkSlateGray * alpha);
+                    Vector2 position3 = titlePosition + new Vector2(Container.Width - 40f + ((lastDir > 0) ? (ValueWiggler.Value * 8f) : 0f), 0f) + (flag2 ? vector : Vector2.Zero);
+                    ActiveFont.DrawOutline(">", position3, new Vector2(0.5f, 0.5f), Vector2.One, color3, 2f, strokeColor);
+                }
+
+
+                if (CurrentMenu != null) {
+                    Vector2 menuPosition = new Vector2(top.X + ItemIndent, top.Y + this.TitleHeight + this.ItemSpacing);
+                    this.menu_Render(menuPosition);
+                }
+            }
+            private static void DrawIcon(Vector2 position, MTexture icon, Vector2 justify, bool outline, Color color, float scale) {
+                if (outline) {
+                    icon.DrawOutlineCentered(position + justify, color);
+                } else {
+                    icon.DrawCentered(position + justify, color, scale);
+                }
+            }
+            #endregion
+        }
     }
 }
