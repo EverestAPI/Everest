b2fe800f2ec9e42308095d98b1eeb9284a9939b4
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2020-05-17 23:16:12 -0700

    Add deactivate functionality to dreamblocks

--- Celeste.Mod.mm/Mod/Entities/ActivateDreamBlocksTrigger.cs
+++ Celeste.Mod.mm/Mod/Entities/ActivateDreamBlocksTrigger.cs
@@ -5,20 +5,31 @@ namespace Celeste.Mod.Entities {
     [CustomEntity("everest/activateDreamBlocksTrigger")]
     class ActivateDreamBlocksTrigger : Trigger {
         private bool rumble;
+        private bool deactivate;
         public ActivateDreamBlocksTrigger(EntityData data, Vector2 offset)
             : base(data, offset) {
             rumble = data.Bool("fullRoutine");
+            deactivate = data.Bool("deactivate");
         }
 
         public override void OnEnter(Player player) {
             Level level = Scene as Level;
-            if (!level.Session.Inventory.DreamDash) {
+            if (!deactivate && !level.Session.Inventory.DreamDash) {
                 level.Session.Inventory.DreamDash = true;
                 foreach (DreamBlock dreamBlock in level.Tracker.GetEntities<DreamBlock>()) {
                     if (rumble)
                         dreamBlock.Add(new Coroutine(dreamBlock.Activate(), true));
                     else
                         dreamBlock.ActivateNoRoutine();
+                }
+            }
+            else if (deactivate && level.Session.Inventory.DreamDash) {
+                level.Session.Inventory.DreamDash = false;
+                foreach (DreamBlock dreamBlock in level.Tracker.GetEntities<DreamBlock>()) {
+                    if (rumble)
+                        dreamBlock.Add(new Coroutine(((patch_DreamBlock) dreamBlock).Deactivate(), true));
+                    else
+                        ((patch_DreamBlock) dreamBlock).DeactivateNoRoutine();
                 }
             }
         }
--- Celeste.Mod.mm/Patches/DreamBlock.cs
+++ Celeste.Mod.mm/Patches/DreamBlock.cs
@@ -11,9 +11,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using Microsoft.Xna.Framework;
+using System.Collections;
 
 namespace Celeste {
     class patch_DreamBlock : DreamBlock {
+        private bool playerHasDreamDash;
+        private LightOcclude occlude;
+        private float whiteHeight;
+        private float whiteFill;
+        private Shaker shaker;
+        private Vector2 shake;
 
         public patch_DreamBlock(EntityData data, Vector2 offset)
             : base(data, offset) {
@@ -29,5 +36,61 @@ namespace Celeste {
             ctor(position, width, height, node, fastMoving, oneUse, false);
         }
 
+        public void DeactivateNoRoutine() {
+            if (playerHasDreamDash) {
+                playerHasDreamDash = false;
+                Setup();
+                if (occlude == null) {
+                    occlude = new LightOcclude(1f);
+                }
+                Add(occlude);
+                whiteHeight = 1f;
+                whiteFill = 0f;
+                if (shaker != null) {
+                    shaker.On = false;
+                }
+                SurfaceSoundIndex = 11;
+            }
+        }
+
+        public IEnumerator Deactivate() {
+            Level level = SceneAs<Level>();
+            yield return 1f;
+            Input.Rumble(RumbleStrength.Light, RumbleLength.Long);
+            if (shaker == null) {
+                shaker = new Shaker(true, delegate (Vector2 t) {
+                    shake = t;
+                });
+            }
+            Add(shaker);
+            shaker.Interval = 0.02f;
+            shaker.On = true;
+            for (float alpha = 0f; alpha < 1f; alpha += Engine.DeltaTime) {
+                whiteFill = Ease.CubeIn(alpha);
+                yield return null;
+            }
+            shaker.On = false;
+            yield return 0.5f;
+            DeactivateNoRoutine();
+            whiteHeight = 1f;
+            whiteFill = 1f;
+            for (float yOffset = 1f; yOffset > 0f; yOffset -= Engine.DeltaTime * 0.5f) {
+                whiteHeight = yOffset;
+                if (level.OnInterval(0.1f)) {
+                    for (int xOffset = 0; xOffset < Width; xOffset += 4) {
+                        level.ParticlesFG.Emit(Strawberry.P_WingsBurst, new Vector2(X + xOffset, Y + Height * whiteHeight + 1f));
+                    }
+                }
+                if (level.OnInterval(0.1f)) {
+                    level.Shake(0.3f);
+                }
+                Input.Rumble(RumbleStrength.Strong, RumbleLength.Short);
+                yield return null;
+            }
+            while (whiteFill > 0f) {
+                whiteFill -= Engine.DeltaTime * 3f;
+                yield return null;
+            }
+        }
     }
 }
