78735d35ef091014b0d606480e779325f90f4bc4
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2020-05-21 18:12:34 -0700

    Add fast activation and deactivation to dreamblocks

--- Celeste.Mod.mm/Patches/DreamBlock.cs
+++ Celeste.Mod.mm/Patches/DreamBlock.cs
@@ -21,6 +21,7 @@ namespace Celeste {
         private float whiteFill;
         private Shaker shaker;
         private Vector2 shake;
+        private int randomSeed = Calc.Random.Next();
 
         public patch_DreamBlock(EntityData data, Vector2 offset)
             : base(data, offset) {
@@ -58,7 +59,7 @@ namespace Celeste {
             yield return 1f;
             Input.Rumble(RumbleStrength.Light, RumbleLength.Long);
             if (shaker == null) {
-                shaker = new Shaker(true, delegate (Vector2 t) {
+                shaker = new Shaker(true, t => {
                     shake = t;
                 });
             }
@@ -87,6 +88,72 @@ namespace Celeste {
                 Input.Rumble(RumbleStrength.Strong, RumbleLength.Short);
                 yield return null;
             }
+            while (whiteFill > 0f) {
+                whiteFill -= Engine.DeltaTime * 3f;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FastDeactivate() {
+            Level level = SceneAs<Level>();
+            yield return null;
+            Input.Rumble(RumbleStrength.Light, RumbleLength.Short);
+            if (shaker == null) {
+                shaker = new Shaker(true, t => {
+                    shake = t;
+                });
+            }
+            Add(shaker);
+            shaker.Interval = 0.02f;
+            shaker.On = true;
+            for (float alpha = 0f; alpha < 1f; alpha += Engine.DeltaTime * 3f) {
+                whiteFill = Ease.CubeIn(alpha);
+                yield return null;
+            }
+            shaker.On = false;
+            yield return 0.1f;
+            DeactivateNoRoutine();
+            whiteHeight = 1f;
+            whiteFill = 1f;
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Width, TopCenter, Vector2.UnitX * Width / 2, Color.White, (float) Math.PI);
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Width, BottomCenter, Vector2.UnitX * Width / 2, Color.White, 0);
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Height, CenterLeft, Vector2.UnitY * Height / 2, Color.White, (float) Math.PI * 1.5f);
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Height, CenterRight, Vector2.UnitY * Height / 2, Color.White, (float) Math.PI / 2);
+            level.Shake(0.3f);
+            yield return 0.1f;
+            while (whiteFill > 0f) {
+                whiteFill -= Engine.DeltaTime * 3f;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FastActivate() {
+            Level level = SceneAs<Level>();
+            yield return null;
+            Input.Rumble(RumbleStrength.Light, RumbleLength.Short);
+            if (shaker == null) {
+                shaker = new Shaker(true, t => {
+                    shake = t;
+                });
+            }
+            Add(shaker);
+            shaker.Interval = 0.02f;
+            shaker.On = true;
+            for (float alpha = 0f; alpha < 1f; alpha += Engine.DeltaTime * 3f) {
+                whiteFill = Ease.CubeIn(alpha);
+                yield return null;
+            }
+            shaker.On = false;
+            yield return 0.1f;
+            ActivateNoRoutine();
+            whiteHeight = 1f;
+            whiteFill = 1f;
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Width, TopCenter, Vector2.UnitX * Width / 2, Color.White, (float) Math.PI);
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Width, BottomCenter, Vector2.UnitX * Width / 2, Color.White, 0);
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Height, CenterLeft, Vector2.UnitY * Height / 2, Color.White, (float) Math.PI * 1.5f);
+            level.ParticlesFG.Emit(Strawberry.P_WingsBurst, (int) Height, CenterRight, Vector2.UnitY * Height / 2, Color.White, (float) Math.PI / 2);
+            level.Shake(0.3f);
+            yield return 0.1f;
             while (whiteFill > 0f) {
                 whiteFill -= Engine.DeltaTime * 3f;
                 yield return null;
