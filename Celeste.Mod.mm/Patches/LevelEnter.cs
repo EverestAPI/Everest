44f82befc9d1232a256c5ecd4f6fa0100831dacb
max480 <max480@free.fr>
2020-05-22 19:17:00 +0200

    Postcard Sound ID: support 'variants' and custom event paths

--- Celeste.Mod.mm/Patches/LevelEnter.cs
+++ Celeste.Mod.mm/Patches/LevelEnter.cs
@@ -106,10 +106,21 @@ namespace Celeste {
             if (string.IsNullOrEmpty(soundId))
                 soundId = "csides";
 
-            string prefix = "event:/ui/main/postcard_";
-            if (int.TryParse(soundId, out int areaId))
-                prefix += "ch";
-            prefix += soundId;
+            string prefix;
+            if (soundId.StartsWith("event:/")) {
+                // sound ID is a FMOD event, take it as is.
+                prefix = soundId;
+            } else if (soundId == "variants") {
+                // sound ID is "variants", this is a special case since it is in the new_content bank.
+                prefix = "event:/new_content/ui/postcard_variants";
+            } else {
+                // if a number, use event:/ui/main/postcard_ch{number}
+                // if not, use event:/ui/main/postcard_{text}
+                prefix = "event:/ui/main/postcard_";
+                if (int.TryParse(soundId, out _))
+                    prefix += "ch";
+                prefix += soundId;
+            }
 
             Add(postcard = new Postcard(message, prefix + "_in", prefix + "_out"));
             yield return postcard.DisplayRoutine();
