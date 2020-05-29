7f15afd722d9d90265448498705f9bdddba3a935
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2020-05-28 21:12:49 -0700

    Make StarJumpBlock act similar to FloatySpaceBlock
    Add "hasRail" option
    Remove visual connection between moving and non-moving blocks
    Add StarJumpThru

---
+++ Celeste.Mod.mm/Mod/Entities/StarJumpThru.cs
@@ -0,0 +1,87 @@
+﻿using FMOD;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Celeste.Mod.Entities {
+    [CustomEntity("everest/starJumpThru")]
+    [Tracked]
+    public class StarJumpThru : JumpThru {
+
+        private bool hasRail;
+
+        public StarJumpThru(EntityData data, Vector2 offset) 
+            : base(data.Position + offset, data.Width, true) {
+            hasRail = data.Bool("hasRail", true);
+
+            Depth = -60;
+            SurfaceSoundIndex = 32;
+        }
+
+        public override void Awake(Scene scene) {
+            base.Awake(scene);
+
+            List<MTexture> railsL = GFX.Game.GetAtlasSubtextures("objects/starjumpBlock/leftrailing");
+            List<MTexture> rails = GFX.Game.GetAtlasSubtextures("objects/starjumpBlock/railing");
+            List<MTexture> railsR = GFX.Game.GetAtlasSubtextures("objects/starjumpBlock/rightrailing");
+            List<MTexture> edgesH = GFX.Game.GetAtlasSubtextures("objects/starjumpBlock/edgeH");
+            List<MTexture> corners = GFX.Game.GetAtlasSubtextures("objects/starjumpBlock/corner");
+
+            for (int i = 0; i < Width; i += 8) {
+                Image edge = new Image(edgesH[0]);
+                edge.CenterOrigin();
+                edge.Position = new Vector2(i + 4, 4f);
+                Add(edge);
+            }
+
+            for (int i = 8; i < Width - 8f; i += 8) {
+                Image edge = new Image(Calc.Random.Choose(edgesH));
+                edge.CenterOrigin();
+                edge.Position = new Vector2(i + 4, 6f);
+                Add(edge);
+
+                if (hasRail) {
+                    Image rail = new Image(rails[mod((int) (X + i) / 8, rails.Count)]);
+                    rail.Position = new Vector2(i, -8f);
+                    Add(rail);
+                }
+            }
+
+            Image img;
+            img = new Image(Calc.Random.Choose(corners));
+            img.Scale.X = -1f;
+            if (hasRail) {
+                Image rail = new Image(railsL[mod((int) X / 8, railsL.Count)]);
+                rail.Position = new Vector2(0f, -8f);
+                Add(rail);
+            }
+            img.CenterOrigin();
+            img.Position = new Vector2(4f, 6f);
+            Add(img);
+
+
+            img = new Image(Calc.Random.Choose(corners));
+            if (hasRail) {
+                Image rail = new Image(railsR[mod((int) (X + Width) / 8 - 1, railsR.Count)]);
+                rail.Position = new Vector2(Width - 8f, -8f);
+                Add(rail);
+            }
+            img.CenterOrigin();
+            img.Position = new Vector2(Width - 4f, 6f);
+            Add(img);
+        }
+
+        private int mod(int x, int m) {
+            return (x % m + m) % m;
+        }
+
+        public override void Render() {
+            Draw.Line(TopLeft, TopRight, Color.White);
+            Draw.Line(TopLeft + Vector2.UnitX, new Vector2(Left + 1f, Top + 2f), Color.White);
+            Draw.Line(TopRight, new Vector2(Right, Top + 2f), Color.White);
+            base.Render();
+        }
+
+    }
+}
--- Celeste.Mod.mm/Patches/StarJumpBlock.cs
+++ Celeste.Mod.mm/Patches/StarJumpBlock.cs
@@ -1,26 +1,131 @@
 ﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
 
-using Celeste.Mod;
 using Celeste.Mod.Entities;
-using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 using Monocle;
 using MonoMod;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace Celeste {
-    // : Solid because base.Awake
+    // : Solid because base.Awake, base.Update
     class patch_StarJumpBlock : Solid {
         private Level level;
+        private float yLerp;
+        private bool sinks;
+        private float sinkTimer;
+
+        public bool HasGroup { get; private set; }
+        public bool MasterOfGroup { get; private set; }
+
+        public List<patch_StarJumpBlock> Group;
+        public Point GroupBoundsMax;
+        public Point GroupBoundsMin;
+        public List<StarJumpThru> Jumpthrus;
+        public Dictionary<Platform, Vector2> Moves;
+
+        private patch_StarJumpBlock master;
+
+        private bool awake;
+
+        private bool hasRail;
 
         public patch_StarJumpBlock(Vector2 position, float width, float height, bool sinks) : base(position, width, height, sinks) {
             // no-op. MonoMod ignores this - we only need this to make the compiler shut up.
+        }
+
+        public extern void orig_ctor(EntityData data, Vector2 offset);
+        [MonoModConstructor]
+        public void ctor(EntityData data, Vector2 offset) {
+            orig_ctor(data, offset);
+
+            hasRail = data.Bool("hasRail", true);
+        }
+
+        private void AddToGroupAndFindChildren(patch_StarJumpBlock from) {
+            if (from.X < GroupBoundsMin.X) {
+                GroupBoundsMin.X = (int) from.X;
+            }
+            if (from.Y < GroupBoundsMin.Y) {
+                GroupBoundsMin.Y = (int) from.Y;
+            }
+            if (from.Right > GroupBoundsMax.X) {
+                GroupBoundsMax.X = (int) from.Right;
+            }
+            if (from.Bottom > GroupBoundsMax.Y) {
+                GroupBoundsMax.Y = (int) from.Bottom;
+            }
+            from.HasGroup = true;
+            Group.Add(from);
+            Moves.Add(from, from.Position);
+            if (from != this) {
+                from.master = this;
+            }
+            foreach (StarJumpThru item in Scene.CollideAll<StarJumpThru>(new Rectangle((int) from.X - 1, (int) from.Y, (int) from.Width + 2, (int) from.Height))) {
+                if (!Jumpthrus.Contains(item)) {
+                    AddJumpThru(item);
+                }
+            }
+            foreach (StarJumpThru item in Scene.CollideAll<StarJumpThru>(new Rectangle((int) from.X, (int) from.Y - 1, (int) from.Width, (int) from.Height + 2))) {
+                if (!Jumpthrus.Contains(item)) {
+                    AddJumpThru(item);
+                }
+            }
+            foreach (Solid solid in Scene.Tracker.GetEntities<StarJumpBlock>()) {
+                patch_StarJumpBlock block = solid as patch_StarJumpBlock;
+                if (!block.HasGroup && block.sinks == sinks && (Scene.CollideCheck(new Rectangle((int) from.X - 1, (int) from.Y, (int) from.Width + 2, (int) from.Height), block) || Scene.CollideCheck(new Rectangle((int) from.X, (int) from.Y - 1, (int) from.Width, (int) from.Height + 2), block))) {
+                    AddToGroupAndFindChildren(block);
+                }
+            }
+        }
+
+        private void AddJumpThru(StarJumpThru jp) {
+            Jumpthrus.Add(jp);
+            Moves.Add(jp, jp.Position);
+            foreach (Solid solid in Scene.Tracker.GetEntities<StarJumpBlock>()) {
+                patch_StarJumpBlock block = solid as patch_StarJumpBlock;
+                if (!block.HasGroup && block.sinks == sinks && Scene.CollideCheck(new Rectangle((int) jp.X - 1, (int) jp.Y, (int) jp.Width + 2, (int) jp.Height), block)) {
+                    AddToGroupAndFindChildren(block);
+                }
+            }
+        }
+
+        private void TryToInitPosition() {
+            if (MasterOfGroup) {
+                foreach (patch_StarJumpBlock block in Group) {
+                    if (!block.awake) {
+                        return;
+                    }
+                }
+                MoveToTarget();
+            } else {
+               master.TryToInitPosition();
+            }
+        }
+
+        private void MoveToTarget() {
+            for (int i = 0; i < 2; i++) {
+                foreach (KeyValuePair<Platform, Vector2> move in Moves) {
+                    JumpThru jumpThru = move.Key as JumpThru;
+                    Solid solid = move.Key as Solid;
+
+                    bool hasRider = false;
+                    if ((jumpThru != null && jumpThru.HasRider()) || (solid != null && solid.HasRider())) {
+                        hasRider = true;
+                    }
+
+                    if ((hasRider || i != 0) && (!hasRider || i != 1)) {
+                        float y = MathHelper.Lerp(move.Value.Y, move.Value.Y + 12f, Ease.SineInOut(yLerp));
+                        move.Key.MoveToY(y);
+                    }
+                }
+            }
+        }
+
+        [MonoModReplace]
+        public bool Open(float x, float y) {
+            Solid block = Scene.CollideFirst<StarJumpBlock>(new Vector2(X + x + 4f, Y + y + 4f));
+            return !(block != null && ((patch_StarJumpBlock) block).sinks == sinks);
         }
 
         public extern void orig_Awake(Scene scene);
@@ -36,6 +141,18 @@ namespace Celeste {
             // TODO: Inner corner textures? Or keep them empty as-is?
 
             base.Awake(scene);
+            awake = true;
+            
+            if (!HasGroup) {
+                MasterOfGroup = true;
+                Moves = new Dictionary<Platform, Vector2>();
+                Group = new List<patch_StarJumpBlock>();
+                Jumpthrus = new List<StarJumpThru>();
+                GroupBoundsMin = new Point((int) X, (int) Y);
+                GroupBoundsMax = new Point((int) Right, (int) Bottom);
+                AddToGroupAndFindChildren(this);
+            }
+            TryToInitPosition();
 
             level = SceneAs<Level>();
 
@@ -54,9 +171,11 @@ namespace Celeste {
                     edge.Position = new Vector2(i + 4, 4f);
                     Add(edge);
 
-                    Image rail = new Image(rails[mod((int) (X + i) / 8, rails.Count)]);
-                    rail.Position = new Vector2(i, -8f);
-                    Add(rail);
+                    if (hasRail) {
+                        Image rail = new Image(rails[mod((int) (X + i) / 8, rails.Count)]);
+                        rail.Position = new Vector2(i, -8f);
+                        Add(rail);
+                    }
                 }
 
                 if (Open(i, Height)) {
@@ -96,9 +215,11 @@ namespace Celeste {
                 img = new Image(Calc.Random.Choose(corners));
                 img.Scale.X = -1f;
 
-                Image rail = new Image(railsL[mod((int) X / 8, railsL.Count)]);
-                rail.Position = new Vector2(0f, -8f);
-                Add(rail);
+                if (hasRail) {
+                    Image rail = new Image(railsL[mod((int) X / 8, railsL.Count)]);
+                    rail.Position = new Vector2(0f, -8f);
+                    Add(rail);
+                }
 
             } else if (Open(-8f, 0f)) {
                 img = new Image(Calc.Random.Choose(edgesV));
@@ -107,9 +228,11 @@ namespace Celeste {
             } else if (Open(0f, -8f)) {
                 img = new Image(Calc.Random.Choose(edgesH));
 
-                Image rail = new Image(rails[mod((int) X / 8, rails.Count)]);
-                rail.Position = new Vector2(0f, -8f);
-                Add(rail);
+                if (hasRail) {
+                    Image rail = new Image(rails[mod((int) X / 8, rails.Count)]);
+                    rail.Position = new Vector2(0f, -8f);
+                    Add(rail);
+                }
             }
 
             if (img != null) {
@@ -123,9 +246,11 @@ namespace Celeste {
             if (Open(Width, 0f) && Open(Width - 8f, -8f)) {
                 img = new Image(Calc.Random.Choose(corners));
 
-                Image rail = new Image(railsR[mod((int) (X + Width) / 8 - 1, railsR.Count)]);
-                rail.Position = new Vector2(Width - 8f, -8f);
-                Add(rail);
+                if (hasRail) {
+                    Image rail = new Image(railsR[mod((int) (X + Width) / 8 - 1, railsR.Count)]);
+                    rail.Position = new Vector2(Width - 8f, -8f);
+                    Add(rail);
+                }
 
             } else if (Open(Width, 0f)) {
                 img = new Image(Calc.Random.Choose(edgesV));
@@ -133,9 +258,11 @@ namespace Celeste {
             } else if (Open(Width - 8f, -8f)) {
                 img = new Image(Calc.Random.Choose(edgesH));
 
-                Image rail = new Image(rails[mod((int) (X + Width) / 8 - 1, rails.Count)]);
-                rail.Position = new Vector2(Width - 8f, -8f);
-                Add(rail);
+                if (hasRail) {
+                    Image rail = new Image(rails[mod((int) (X + Width) / 8 - 1, rails.Count)]);
+                    rail.Position = new Vector2(Width - 8f, -8f);
+                    Add(rail);
+                }
             }
 
             if (img != null) {
@@ -185,9 +312,47 @@ namespace Celeste {
             }
         }
 
+        public extern void orig_Update();
+        public override void Update() {
+            if (SceneAs<Level>().Session.Area.GetLevelSet() == "Celeste") {
+                orig_Update();
+                return;
+            }
+
+            base.Update();
+            if (MasterOfGroup && sinks) {
+                bool hasRider = false;
+                foreach (patch_StarJumpBlock block in Group) {
+                    if (block.HasPlayerRider()) {
+                        hasRider = true;
+                        break;
+                    }
+                }
+                if (!hasRider) {
+                    foreach(StarJumpThru jumpThru in Jumpthrus) {
+                        if (jumpThru.HasPlayerRider()) {
+                            hasRider = true;
+                            break;
+                        }
+                    }
+                }
+                if (hasRider) {
+                    sinkTimer = 0.1f;
+                } else if (sinkTimer > 0f) {
+                    sinkTimer -= Engine.DeltaTime;
+                }
+                if (sinkTimer > 0f) {
+                    yLerp = Calc.Approach(yLerp, 1f, 1f * Engine.DeltaTime);
+                } else {
+                    yLerp = Calc.Approach(yLerp, 0f, 1f * Engine.DeltaTime);
+                }
+                MoveToTarget();
+            }
+        }
+
         public extern void orig_Render();
         public override void Render() {
-            if (this.SceneAs<Level>().Session.Area.GetLevelSet() == "Celeste") {
+            if (SceneAs<Level>().Session.Area.GetLevelSet() == "Celeste") {
                 orig_Render();
                 return;
             }
@@ -196,10 +361,10 @@ namespace Celeste {
             // which is color-customizable and doesn't include forced music.
             // Prioritizes the Everest controller if found, though you shouldn't be using both at once.
 
-            StarJumpController vanillaController = this.Scene.Tracker.GetEntity<StarJumpController>();
-            StarClimbGraphicsController everestController = this.Scene.Tracker.GetEntity<StarClimbGraphicsController>();
+            StarJumpController vanillaController = Scene.Tracker.GetEntity<StarJumpController>();
+            StarClimbGraphicsController everestController = Scene.Tracker.GetEntity<StarClimbGraphicsController>();
 
-            Vector2 cameraPos = this.level.Camera.Position.Floor();
+            Vector2 cameraPos = level.Camera.Position.Floor();
             VirtualRenderTarget blockFill = null;
 
             if (everestController != null)
@@ -210,8 +375,8 @@ namespace Celeste {
             if (blockFill != null) {
                 Draw.SpriteBatch.Draw(
                     blockFill,
-                    this.Position,
-                    new Rectangle?(new Rectangle((int) (this.X - cameraPos.X), (int) (this.Y - cameraPos.Y), (int) this.Width, (int) this.Height)),
+                    Position,
+                    new Rectangle?(new Rectangle((int) (X - cameraPos.X), (int) (Y - cameraPos.Y), (int) Width, (int) Height)),
                     Color.White
                 );
             }
@@ -221,8 +386,5 @@ namespace Celeste {
 
         [MonoModIgnore]
         private extern int mod(int v, int count);
-
-        [MonoModIgnore]
-        private extern bool Open(float index, float v);
     }
 }
