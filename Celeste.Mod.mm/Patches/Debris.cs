7b1fe66090bbd996479ee9490959e0f4393a6e55
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2020-06-20 18:51:21 -0700

    Add custom debris texture to tilesets

--- Celeste.Mod.mm/Patches/Debris.cs
+++ Celeste.Mod.mm/Patches/Debris.cs
@@ -1,22 +1,26 @@
 ﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
 
-using Celeste.Mod;
-using Microsoft.Xna.Framework.Input;
-using Monocle;
-using MonoMod;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using Microsoft.Xna.Framework;
+using Monocle;
 
 namespace Celeste {
     class patch_Debris : Debris {
 
+        private Image image;
+
         public Debris Init(Vector2 pos, char tileset) {
             return Init(pos, tileset, true);
+        }
+
+        public extern Debris orig_Init(Vector2 pos, char tileset, bool playSound = true);
+        public new Debris Init(Vector2 pos, char tileset, bool playSound) {
+            patch_Debris debris = (patch_Debris) orig_Init(pos, tileset, playSound);
+
+            if (((patch_Autotiler) GFX.FGAutotiler).TryGetCustomDebris(out string path, tileset))
+                debris.image.Texture = GFX.Game["debris/" + path];
+
+            return debris;
         }
 
     }
