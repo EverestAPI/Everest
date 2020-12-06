34ecb68987cab2fe25c1815a9c3aafed5596aaa4
max480 <max480@free.fr>
2020-12-06 15:16:53 +0100

    Fix potential crashes linked to MInput.UpdateVirtualInputs on hot mod loading

--- Celeste.Mod.mm/Patches/Monocle/MInput.cs
+++ Celeste.Mod.mm/Patches/Monocle/MInput.cs
@@ -3,9 +3,21 @@
 #pragma warning disable CS0169 // The field is never used
 
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Monocle {
     class patch_MInput {
+        // vanilla internal field
+        internal static List<VirtualInput> VirtualInputs;
+
+        // lock VirtualInputs so that the VirtualInput class doesn't modify it while UpdateVirtualInputs() iterates it.
+        private static extern void orig_UpdateVirtualInputs();
+        private static void UpdateVirtualInputs() {
+            lock (VirtualInputs) {
+                orig_UpdateVirtualInputs();
+            }
+        }
+
         public class patch_KeyboardData {
             public extern bool orig_Check(Keys key);
 
