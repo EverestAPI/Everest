836b15da89d7bba8fc201745bad4e51370f6c309
Jade Macho <nuda1998@gmail.com>
2021-02-08 17:27:02 +0100

    Fix "delayed swaps" in coroutines, except for vanilla ones

--- Celeste.Mod.mm/Patches/Monocle/Coroutine.cs
+++ Celeste.Mod.mm/Patches/Monocle/Coroutine.cs
@@ -1,17 +1,49 @@
 ﻿#pragma warning disable CS0414 // The field is assigned but its value is never used
+#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
 
+using MonoMod;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Monocle {
     class patch_Coroutine : Coroutine {
 
         // We're effectively in Coroutine, but still need to "expose" private fields to our mod.
         private float waitTimer;
+        private Stack<IEnumerator> enumerators;
+
+        /// <summary>
+        /// Force this coroutine to mimic vanilla behavior:<br></br>
+        /// When yield returning a new IEnumerator or when a previously IEnumerator finishes,
+        /// the next IEnumerator runs delayed by one frame.<br></br>
+        /// <br></br>
+        /// To describe the behavior of this field, imagine the following code replacing all yield returns of IEnumerators:<br></br>
+        /// <code>
+        /// IEnumerator next = Nested(...);<br></br>
+        /// if (ForceDelayedSwap) yield return null;<br></br>
+        /// while (next.MoveNext()) yield return next.Current;<br></br>
+        /// if (ForceDelayedSwap) yield return null;<br></br>
+        /// // Control is returned to your code here.<br></br>
+        /// </code>
+        /// </summary>
+        public bool ForceDelayedSwap;
 
         /// <summary>
         /// Forcibly set the timer to 0 to jump to the next "step."
         /// </summary>
         public void Jump() {
             waitTimer = 0;
+        }
+
+        public extern void orig_Update();
+        public override void Update() {
+            IEnumerator prev, next;
+            do {
+                prev = enumerators.Count > 0 ? enumerators.Peek() : null;
+                orig_Update();
+                next = enumerators.Count > 0 ? enumerators.Peek() : null;
+            } while (prev != next && next != null && !(prev?.GetType()?.Assembly == typeof(Engine).Assembly || ForceDelayedSwap));
         }
 
     }
