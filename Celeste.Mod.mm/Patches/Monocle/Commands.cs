4bb6805a33398cb0210e7fdb75b63712abd141bd
Audrey Dutcher <audrey@rhelmot.io>
2021-02-16 00:47:21 -0700

    Add function keys

--- Celeste.Mod.mm/Patches/Monocle/Commands.cs
+++ Celeste.Mod.mm/Patches/Monocle/Commands.cs
@@ -286,6 +286,20 @@ namespace Monocle {
                     currentText = seekIndex == -1 ? "" : commandHistory[seekIndex];
                     charIndex = currentText.Length;
                     break;
+                case Keys.F1:
+                case Keys.F2:
+                case Keys.F3:
+                case Keys.F4:
+                case Keys.F5:
+                case Keys.F6:
+                case Keys.F7:
+                case Keys.F8:
+                case Keys.F9:
+                case Keys.F10:
+                case Keys.F11:
+                case Keys.F12:
+                    ExecuteFunctionKeyAction(key - Keys.F1);
+                    break;
             }
         }
 
