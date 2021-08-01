24450f9d0ec44f977f3ff305aac7c9d2af9fbe50
coloursofnoise <coloursofthenoise@gmail.com>
2021-08-01 13:22:48 -0700

    Inject build number Name during prebuild pipeline (#353)

--- azure-pipelines-prebuild.ps1
+++ azure-pipelines-prebuild.ps1
@@ -2,3 +2,13 @@
 $BuildNumber = [string]([int]$env:Build_BuildId + [int]$env:Build_BuildIdOffset)
 $EverestPath = [io.path]::combine('Celeste.Mod.mm', 'Mod', 'Everest', 'Everest.cs')
 (Get-Content $EverestPath) -replace '(?<=public readonly static string VersionString = ")[^"]*', "1.$BuildNumber.0-azure-$(($env:BUILD_SOURCEVERSION).Substring(0, 5))" | Set-Content $EverestPath
+
+# Currently unstable/in development
+$HelperPath = [io.path]::combine('Celeste.Mod.mm', 'Mod', 'Helpers', 'EverestVersion.cs')
+echo @"
+namespace Celeste.Mod.Helpers {
+    internal static class EverestBuild$BuildNumber {
+        public static string EverestBuild = "EverestBuild$BuildNumber";
+    }
+}
+"@ | Set-Content $HelperPath
