731deb9725137940dd67fdc6d764c36ec870f6fd
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2021-08-27 22:49:11 -0700

    Add lib-stripped artifact to build pipeline

--- azure-pipelines-postbuild.ps1
+++ azure-pipelines-postbuild.ps1
@@ -21,3 +21,36 @@ Move-Item -Path "olympus-build.zip" -Destination $ZIP
 
 Write-Output "Building Olympus metadata artifact"
 Write-Output (Get-Item -Path $ZIP).length | Out-File -FilePath $OLYMPUS/meta/size.txt
+
+# lib-stripped setup
+$LIB_STRIPPED="$env:Build_ArtifactStagingDirectory/lib-stripped"
+if ($LIB_STRIPPED -eq "/lib-stripped/") {
+	$LIB_STRIPPED = "./tmp-lib-stripped/"
+}
+
+$ZIP="$LIB_STRIPPED/build/build.zip"
+
+Write-Output "Installing SteamRE DepotDownloader"
+Invoke-WebRequest -URI 'https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.4.4/depotdownloader-2.4.4.zip' -OutFile "$env:Agent_TempDirectory/depotdownloader.zip"
+Expand-Archive -Path "$env:Agent_TempDirectory/depotdownloader.zip" -Destination $env:Agent_ToolsDirectory
+
+Write-Output "Downloading Celeste package"
+dotnet DepotDownloader.dll -app 504230 -beta opengl -username $env:STEAM_USERNAME -password $env:STEAM_PASSWORD -dir $LIB_STRIPPED
+
+Write-Output "Applying Everest patch"
+Copy-Item -Path "$env:Build_ArtifactStagingDirectory/main/*" -Destination $LIB_STRIPPED -Recurse
+"$LIB_STRIPPED"/Miniinstaller.exe
+
+Write-Output "Generating stripped files"
+$files = Get-ChildItem -Path $LIB_STRIPPED* -Include *.dll,*.exe
+foreach ($dll in $files) {
+	mono-cil-strip -q $dll
+}
+
+Write-Output "Building lib-stripped artifact"
+$compress = @{
+	Path = "$LIB_STRIPPED/*.dll", "$LIB_STRIPPED/*.exe"
+	CompressionLevel = "Optimal"
+	DestinationPath = "$ZIP"
+}
+Compress-Archive @compress
