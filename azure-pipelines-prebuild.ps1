56e77edb44695d35049f8a25dd5633ecb26281ac
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2021-09-29 01:31:58 -0700

    pipeline test logging

--- azure-pipelines-prebuild.ps1
+++ azure-pipelines-prebuild.ps1
@@ -1,7 +1,12 @@
 # Make the Everest version match the build number.
+echo $env:Build_BuildId
+echo $env:Build_BuildIdOffset
 $BuildNumber = [string]([int]$env:Build_BuildId + [int]$env:Build_BuildIdOffset)
 $EverestPath = [io.path]::combine($env:Build_SourcesDirectory, 'Celeste.Mod.mm', 'Mod', 'Everest', 'Everest.cs')
+echo $BuildNumber
 (Get-Content $EverestPath) -replace '(?<=public readonly static string VersionString = ")[^"]*', "1.$BuildNumber.0-azure-$(($env:BUILD_SOURCEVERSION).Substring(0, 5))" | Set-Content $EverestPath
+echo ""
+cat $EverestPath
 
 # Currently unstable/in development
 $HelperPath = [io.path]::combine($env:Build_SourcesDirectory, 'Celeste.Mod.mm', 'Mod', 'Helpers', 'EverestVersion.cs')
