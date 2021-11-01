9382dbe8a22fb72bd4026eb8f5514bdd93f6a9a9
Jade Macho <nuda1998@gmail.com>
2021-11-01 21:33:13 +0100

    Remove UpdatedOnce check from MainThreadHelper GetForceQueue

--- Celeste.Mod.mm/Mod/Helpers/MainThreadHelper.cs
+++ Celeste.Mod.mm/Mod/Helpers/MainThreadHelper.cs
@@ -89,7 +89,7 @@ namespace Celeste.Mod {
             PreventQueueOverload();
 
             lock (Queue) {
-                MaybeAwaitable<T> awaitable = new MaybeAwaitable<T>(() => UpdatedOnce);
+                MaybeAwaitable<T> awaitable = new MaybeAwaitable<T>();
                 Queue.Enqueue(() => awaitable.SetResult(f != null ? f.Invoke() : default));
                 return awaitable;
             }
@@ -113,7 +113,7 @@ namespace Celeste.Mod {
 
                 T result = default;
                 Task<T> proxy = new Task<T>(() => result);
-                MaybeAwaitable<T> awaitable = new MaybeAwaitable<T>(proxy.GetAwaiter(), () => UpdatedOnce);
+                MaybeAwaitable<T> awaitable = new MaybeAwaitable<T>(proxy.GetAwaiter());
                 EnqueuedWaiting[key] = awaitable;
 
                 Queue.Enqueue(() => {
