4fe96f7d8af0acc3257b8c22be673385b5516984
WEGFan <goofans@qq.com>
2021-12-24 01:54:03 +0800

    Disable anchors in serialized yaml

--- Celeste.Mod.mm/Mod/Helpers/YamlHelper.cs
+++ Celeste.Mod.mm/Mod/Helpers/YamlHelper.cs
@@ -6,7 +6,7 @@ namespace Celeste.Mod {
     public static class YamlHelper {
 
         public static IDeserializer Deserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().Build();
-        public static ISerializer Serializer = new SerializerBuilder().ConfigureDefaultValuesHandling(DefaultValuesHandling.Preserve).Build();
+        public static ISerializer Serializer = new SerializerBuilder().ConfigureDefaultValuesHandling(DefaultValuesHandling.Preserve).DisableAliases().Build();
 
         /// <summary>
         /// Builds a deserializer that will provide YamlDotNet with the given object instead of creating a new one.
