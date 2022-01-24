6ad124cb492d494aa5981cfda788180ba7851deb
Viv-0 <reserve.power.2514.2@gmail.com>
2022-01-23 20:17:42 -0500

    Added summary for the interfaces

--- Celeste.Mod.mm/Mod/Entities/IPostUpdateComponent.cs
+++ Celeste.Mod.mm/Mod/Entities/IPostUpdateComponent.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Celeste.Mod.Entities {
+
+    /// <summary>
+    /// All Components added to an Entity that inherit this interface will run the code inside PreUpdate after the Entity completes running its Update call.
+    /// </summary>
     public interface IPostUpdateComponent {
         void PostUpdate();
     }
--- Celeste.Mod.mm/Mod/Entities/IPreUpdateComponent.cs
+++ Celeste.Mod.mm/Mod/Entities/IPreUpdateComponent.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Celeste.Mod.Entities {
+
+    /// <summary>
+    /// All Components added to an Entity that inherit this interface will run the code inside PreUpdate before the Entity runs its Update call.
+    /// </summary>
     public interface IPreUpdateComponent {
         void PreUpdate();
     }
