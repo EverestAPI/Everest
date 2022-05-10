3dee55a6863bd235d710b52a888d1bdca7cd9d82
caeyo <oppermancarter@gmail.com>
2022-05-10 21:43:42 +1000

    Make IntroCrushers respect tileset sounds

--- Celeste.Mod.mm/Patches/IntroCrusher.cs
+++ Celeste.Mod.mm/Patches/IntroCrusher.cs
@@ -26,6 +26,7 @@ namespace Celeste {
             levelFlags = data.Attr("flags");
 
             string tiletype = data.Attr("tiletype");
+            SurfaceSoundIndex = SurfaceIndex.TileToIndex[tiletype[0]];
             if (!string.IsNullOrEmpty(tiletype)) {
                 Remove(tilegrid);
                 Add(tilegrid = GFX.FGAutotiler.GenerateBox(tiletype[0], data.Width / 8, data.Height / 8).TileGrid);
