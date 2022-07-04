b9d85481958e4362133a8c963585618bf04e92d7
Microlith57 <microlith57@gmail.com>
2022-07-05 09:58:56 +1200

    cache `Session.LevelData` for performance

--- Celeste.Mod.mm/Patches/Session.cs
+++ Celeste.Mod.mm/Patches/Session.cs
@@ -6,6 +6,11 @@ namespace Celeste {
     public class patch_Session {
         public extern void orig_ctor(AreaKey area, string checkpoint = null, AreaStats oldStats = null);
 
+        [MonoModIgnore]
+        public string Level;
+
+        private LevelData levelData;
+
         [MonoModConstructor]
         public void ctor(AreaKey area, string checkpoint = null, AreaStats oldStats = null) {
             AreaData areaData = AreaData.Get(area);
@@ -15,6 +20,14 @@ namespace Celeste {
                 areaData.OverrideASideMeta(area.Mode);
             }
             orig_ctor(area, checkpoint, oldStats);
+        }
+
+        public extern LevelData orig_get_LevelData();
+        public LevelData get_LevelData() {
+            if (levelData is null || levelData.Name != Level) {
+                levelData = orig_get_LevelData();
+            }
+            return levelData;
         }
     }
 }
