4278acc9b74f17d16e7e300456fafde342879405
coloursofnoise <coloursofthenoise@gmail.com>
2022-08-06 17:48:12 -0700

    Rewrite azure build pipeline

--- .azure-pipelines/prebuild.ps1
+++ .azure-pipelines/prebuild.ps1
@@ -3,7 +3,7 @@ $BuildNumber = [string]([int]$env:BUILD_BUILDID + [int]$env:BUILD_BUILDIDOFFSET)
 $EverestPath = [io.path]::combine($env:BUILD_SOURCESDIRECTORY, 'Celeste.Mod.mm', 'Mod', 'Everest', 'Everest.cs')
 (Get-Content $EverestPath) -replace '(?<=public readonly static string VersionString = ")[^"]*', "1.$BuildNumber.0-azure-$(($env:BUILD_SOURCEVERSION).Substring(0, 5))" | Set-Content $EverestPath
 
-# Currently unstable/in development
+# Add Everest version as string constant for non-.NET dll parsers.
 $HelperPath = [io.path]::combine($env:BUILD_SOURCESDIRECTORY, 'Celeste.Mod.mm', 'Mod', 'Helpers', 'EverestVersion.cs')
 echo @"
 namespace Celeste.Mod.Helpers {
