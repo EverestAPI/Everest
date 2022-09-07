73da8e759261f19a2df9e3c3a7af40584931c336
bigkahuna443 <jonathan.luke.long@gmail.com>
2022-09-07 01:36:27 -0400

    Remove unused usings

--- Celeste.Mod.mm/Mod/Registry/DecalRegistry.cs
+++ Celeste.Mod.mm/Mod/Registry/DecalRegistry.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Monocle;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Xml;
