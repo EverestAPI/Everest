877ced225715dd3d58888d0ff6ee54c4a959a0a2
microlith57 <microlith57@gmail.com>
2022-11-24 17:39:32 +1300

    add ambience volume trigger
    ported from MaxHelpingHand; uses AudioState to store the volume.
    
    Co-authored-by: max480 <max480@free.fr>

--- Celeste.Mod.mm/Patches/AudioState.cs
+++ Celeste.Mod.mm/Patches/AudioState.cs
@@ -1,9 +1,50 @@
-﻿namespace Celeste {
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+using MonoMod;
+
+namespace Celeste {
     class patch_AudioState : AudioState {
+
+        public float AmbienceVolume;
+
+        [MonoModConstructor]
+        public patch_AudioState() {
+            AmbienceVolume = 1f;
+        }
+
+        [MonoModConstructor]
+        public patch_AudioState(AudioTrackState music, AudioTrackState ambience) {
+            if (music != null) {
+                Music = music.Clone();
+            }
+            if (ambience != null) {
+                Ambience = ambience.Clone();
+            }
+            AmbienceVolume = 1f;
+        }
+
+        [MonoModConstructor]
+        public patch_AudioState(string music, string ambience) {
+            Music.Event = music;
+            Ambience.Event = ambience;
+            AmbienceVolume = 1f;
+        }
+
+        public extern void orig_Apply(bool forceSixteenthNoteHack = false);
+        public new void Apply(bool forceSixteenthNoteHack = false) {
+            orig_Apply();
+            Audio.CurrentAmbienceEventInstance?.setVolume(AmbienceVolume);
+        }
 
         public void Apply() {
             Apply(false);
         }
 
+        public extern patch_AudioState orig_Clone();
+        public new patch_AudioState Clone() {
+            patch_AudioState audioState = orig_Clone();
+            audioState.AmbienceVolume = AmbienceVolume;
+            return audioState;
+        }
     }
 }
