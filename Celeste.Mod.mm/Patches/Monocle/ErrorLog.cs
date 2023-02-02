7ebd278e960abcb79f555b12e180e8e289bac8e4
Popax21 <popa21@popa21.at>
2023-02-02 01:04:24 +0100

    Fix error log not opening

--- Celeste.Mod.mm/Patches/Monocle/ErrorLog.cs
+++ Celeste.Mod.mm/Patches/Monocle/ErrorLog.cs
@@ -9,6 +9,7 @@ using Mono.Cecil;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using MonoMod.InlineRT;
+using System.Diagnostics;
 
 namespace Monocle {
     class patch_ErrorLog {
@@ -24,6 +25,8 @@ namespace Monocle {
         [PatchErrorLogWrite] // ... except for manually manipulating the method via MonoModRules
         public static extern void Write(string str);
 
+        [MonoModIgnore]
+        [PatchErrorLogOpen]
         public static extern void orig_Open();
         public static void Open() {
             if (Environment.GetEnvironmentVariable("EVEREST_NO_ERRORLOG_ON_CRASH") != "1" && CoreModule.Settings.OpenErrorLogOnCrash)
@@ -40,6 +43,12 @@ namespace MonoMod {
     [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchErrorLogWrite))]
     class PatchErrorLogWriteAttribute : Attribute { }
 
+    /// <summary>
+    /// Find call to Process.Start, and set UseShellExecute flag
+    /// </summary>
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchErrorLogOpen))]
+    class PatchErrorLogOpenAttribute : Attribute { }
+
     static partial class MonoModRules {
 
         public static void PatchErrorLogWrite(ILContext context, CustomAttribute attrib) {
@@ -48,8 +57,8 @@ namespace MonoMod {
 
             /* We expect something similar enough to the following:
             call     class Monocle.Engine Monocle.Engine::get_Instance() // We're here
-            ldfld    class [mscorlib] System.Version Monocle.Engine::Version 
-            callvirt instance string[mscorlib] System.Object::ToString() 
+            ldfld    class [mscorlib] System.Version Monocle.Engine::Version
+            callvirt instance string[mscorlib] System.Object::ToString()
 
             Note that MonoMod requires the full type names (System.String instead of string)
             */
@@ -62,6 +71,43 @@ namespace MonoMod {
             // Remove all that and replace with our own string.
             cursor.RemoveRange(3);
             cursor.Emit(OpCodes.Call, m_Everest_get_VersionCelesteString);
+        }
+
+        public static void PatchErrorLogOpen(ILContext context, CustomAttribute attrib) {
+            ILCursor cursor = new ILCursor(context);
+            cursor.GotoNext(instr => instr.MatchCall("System.Diagnostics.Process", "Start"));
+            cursor.Remove();
+
+            /*
+            Process proc = new Process() { StartInfo = new ProcessStartInfo(file) { UseShellExecute = true } };
+            proc.Start();
+            */
+
+            TypeDefinition t_Process = MonoModRule.Modder.FindType("System.Diagnostics.Process").Resolve();
+            TypeDefinition t_StartInfo = MonoModRule.Modder.FindType("System.Diagnostics.ProcessStartInfo").Resolve();
+
+            VariableDefinition fileVar = new VariableDefinition(MonoModRule.Modder.FindType("System.String").Resolve());
+            context.Body.Variables.Add(fileVar);
+            cursor.Emit(OpCodes.Stloc, fileVar);
+
+            // Create new process
+            cursor.Emit(OpCodes.Newobj, MonoModRule.Modder.Module.ImportReference(t_Process.FindMethod("System.Void .ctor()")));
+            cursor.Emit(OpCodes.Dup);
+            cursor.Emit(OpCodes.Dup);
+
+            // Set StartInfo
+            cursor.Emit(OpCodes.Ldloc, fileVar);
+            cursor.Emit(OpCodes.Newobj, MonoModRule.Modder.Module.ImportReference(t_StartInfo.FindMethod("System.Void .ctor(System.String)")));
+            cursor.Emit(OpCodes.Dup);
+
+            cursor.Emit(OpCodes.Ldc_I4_1);
+            cursor.Emit(OpCodes.Callvirt, MonoModRule.Modder.Module.ImportReference(t_StartInfo.FindMethod("set_UseShellExecute")));
+
+            cursor.Emit(OpCodes.Callvirt, MonoModRule.Modder.Module.ImportReference(t_Process.FindMethod("set_StartInfo")));
+
+            // Start the process
+            cursor.Emit(OpCodes.Callvirt, MonoModRule.Modder.Module.ImportReference(t_Process.FindMethod("System.Boolean Start()")));
+            cursor.Emit(OpCodes.Pop);
         }
 
     }
