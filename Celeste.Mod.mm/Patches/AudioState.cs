daf05c597e2f593f5e79eaf17b73488c2d28de4d
microlith57 <microlith57@gmail.com>
2023-02-03 12:08:43 +1300

    remove unnecessary patches; fix argument not being passed

--- Celeste.Mod.mm/Patches/AudioState.cs
+++ Celeste.Mod.mm/Patches/AudioState.cs
@@ -1,35 +1,13 @@
 #pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
 
-using MonoMod;
-
 namespace Celeste {
     class patch_AudioState : AudioState {
 
         public float? AmbienceVolume;
 
-        [MonoModConstructor]
-        [MonoModReplace]
-        public void ctor() {
-            AmbienceVolume = null;
-        }
-
-        public extern void orig_ctor(AudioTrackState music, AudioTrackState ambience);
-        [MonoModConstructor]
-        public void ctor(AudioTrackState music, AudioTrackState ambience) {
-            orig_ctor(music, ambience);
-            AmbienceVolume = null;
-        }
-
-        public extern void orig_ctor(string music, string ambience);
-        [MonoModConstructor]
-        public void ctor(string music, string ambience) {
-            orig_ctor(music, ambience);
-            AmbienceVolume = null;
-        }
-
         public extern void orig_Apply(bool forceSixteenthNoteHack = false);
         public new void Apply(bool forceSixteenthNoteHack = false) {
-            orig_Apply();
+            orig_Apply(forceSixteenthNoteHack);
             if (AmbienceVolume.HasValue)
                 Audio.CurrentAmbienceEventInstance?.setVolume(AmbienceVolume.Value);
         }
