d124533848c46c55380ae373be0e1597d00a47b4
Popax21 <popa21@popa21.at>
2023-02-25 19:26:46 +0100

    Drop XNA support completely

--- Celeste.Mod.mm/MonoModRules.Game.cs
+++ Celeste.Mod.mm/MonoModRules.Game.cs
@@ -138,8 +138,11 @@ namespace MonoMod {
         }
 
         public static void GamePreProcessor(MonoModder modder) {
-            // Relink against FNA
-            RelinkAgainstFNA(modder);
+            // Check if this is an XNA build
+            if (modder.Module.AssemblyReferences.Any(asmRef => asmRef.Name.StartsWith("Microsoft.Xna.Framework")))
+                throw new Exception("Only FNA Celeste builds are supported");
+
+            EnsureFNAIsResolvable(modder);
 
             static void VisitType(TypeDefinition type) {
                 // Remove readonly attribute from all static fields
