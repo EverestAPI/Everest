4dbd1205d02e737c01c83b426a5de5a2fdc6733a
nhruo <nhruo123@gmail.com>
2023-06-29 15:12:47 +0300

    fixed FNA color patch

--- Celeste.Mod.mm/Patches/FNA/Color.cs
+++ Celeste.Mod.mm/Patches/FNA/Color.cs
@@ -6,7 +6,10 @@ namespace Microsoft.Xna.Framework {
 
         // The following signatures existed in older versions of FNA when they shouldn't have.
 
+        [MonoModLinkFrom("System.Void Microsoft.Xna.Framework.Color::.ctor(Microsoft.Xna.Framework.Color,System.Int32)")]
         public static Color NewColor(Color color, int alpha) => new Color(color.R, color.G, color.B, alpha);
+
+        [MonoModLinkFrom("System.Void Microsoft.Xna.Framework.Color::.ctor(Microsoft.Xna.Framework.Color,System.Single)")]
         public static Color NewColor(Color color, float alpha) => new Color(color.R, color.G, color.B, (byte) alpha * 255);
 
     }
