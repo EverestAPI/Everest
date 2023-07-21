012b1e0ac58f9491fa7ccb514e12f866e0d2af73
nhruo <nhruo123@gmail.com>
2023-07-21 19:30:01 +0300

    added Clamp to ctor just like in the original func

--- Celeste.Mod.mm/Patches/FNA/Color.cs
+++ Celeste.Mod.mm/Patches/FNA/Color.cs
@@ -6,19 +6,19 @@ namespace Microsoft.Xna.Framework {
 
         // The following signatures existed in older versions of FNA when they shouldn't have.
 
+
         [MonoModConstructor] // we want to call the original constructor
         [MonoModIgnore] // this is necessary to prevent Monomod from generating redundant orig_ctor
         public extern void ctor(int r, int g, int b, int alpha);
 
         [MonoModConstructor]
         public void ctor(Color color, int alpha) {
-            ctor(color.R, color.G, color.B, alpha);
+            ctor(color.R, color.G, color.B, (byte) int.Clamp(alpha, byte.MinValue, byte.MaxValue));
         }
 
         [MonoModConstructor]
         public void ctor(Color color, float alpha) {
-            ctor(color.R, color.G, color.B, (byte) (alpha * 255));
+            ctor(color.R, color.G, color.B, (byte) float.Clamp(alpha * 255, byte.MinValue, byte.MaxValue));
         }
-
     }
 }
