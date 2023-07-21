0367a9aba8fa1790cafea5a3dcc201e5c578b36a
Popax21 <popa21@popa21.at>
2023-07-21 03:27:58 +0200

    Fix more SineWave float jank

--- Celeste.Mod.mm/Patches/Monocle/SineWave.cs
+++ Celeste.Mod.mm/Patches/Monocle/SineWave.cs
@@ -4,6 +4,23 @@ using System;
 namespace Monocle {
     class patch_SineWave : SineWave {
 
+        private float counter;
+
+        public new float Value { [MonoModIgnore] get; [MonoModIgnore] private set; }
+        public new float ValueOverTwo { [MonoModIgnore] get; [MonoModIgnore] private set; }
+        public new float TwoValue { [MonoModIgnore] get; [MonoModIgnore] private set; }
+
+        public new float Counter {
+            [MonoModReplace] get => counter;
+            [MonoModReplace] set {
+                // Fix TAS desyncs by patching .NET FW float jank
+                counter = (float) (((double) value + (double) (float) (Math.PI * 8f)) % (double) (float) (Math.PI * 8f));
+                Value = (float) Math.Sin(counter);
+                ValueOverTwo = (float) Math.Sin(counter / 2f);
+                TwoValue = (float) Math.Sin(counter * 2f);
+            }
+        }
+
         [MonoModLinkTo("Monocle.SineWave", "System.Void .ctor(System.Single,System.Single)")]
         [MonoModForceCall]
         [MonoModRemove]
@@ -17,8 +34,17 @@ namespace Monocle {
 
         [MonoModReplace]
         public new SineWave Randomize() {
+            // Fix TAS desyncs by patching .NET FW float jank
             Counter = (float) ((double) Calc.Random.NextFloat() * (double) (float) (Math.PI * 2f) * 2.0);
             return this;
+        }
+
+        [MonoModReplace]
+        public override void Update() {
+            // Fix TAS desyncs by patching .NET FW float jank
+            Counter += (float) ((double) (float) (Math.PI * 2f) * (double) Frequency * (double) Rate * (double) (UseRawDeltaTime ? Engine.RawDeltaTime : Engine.DeltaTime));
+            if (OnUpdate != null)
+                OnUpdate(Value);
         }
 
     }
