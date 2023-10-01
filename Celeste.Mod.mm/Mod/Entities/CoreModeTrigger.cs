5a08285529d9a4ea3a4aa4887c34567233af725e
microlith57 <microlith57@gmail.com>
2023-10-02 12:45:51 +1300

    add toggle option to core mode trigger

--- Celeste.Mod.mm/Mod/Entities/CoreModeTrigger.cs
+++ Celeste.Mod.mm/Mod/Entities/CoreModeTrigger.cs
@@ -3,20 +3,41 @@
 namespace Celeste.Mod.Entities {
     [CustomEntity("everest/coreModeTrigger", "cavern/coremodetrigger")]
     public class CoreModeTrigger : Trigger {
-        private readonly Session.CoreModes mode;
+        private enum Modes {
+            None,
+            Hot,
+            Cold,
+            Toggle
+        }
+
+        private readonly Modes mode;
         private readonly bool playEffects;
 
-        public CoreModeTrigger(EntityData data, Vector2 offset) 
+        public CoreModeTrigger(EntityData data, Vector2 offset)
             : base(data, offset) {
-            mode = data.Enum("mode", Session.CoreModes.None);
+            mode = data.Enum("mode", Modes.None);
             playEffects = data.Bool("playEffects", true);
         }
 
         public override void OnEnter(Player player) {
             Level level = Scene as Level;
-            if (level.CoreMode == mode)
+
+            Session.CoreModes newMode = Session.CoreModes.None;
+
+            if (mode == Modes.Toggle) {
+                if (level.CoreMode == Session.CoreModes.Hot)
+                    newMode = Session.CoreModes.Cold;
+                else if (level.CoreMode == Session.CoreModes.Cold)
+                    newMode = Session.CoreModes.Hot;
+            } else {
+                newMode = (Session.CoreModes) mode;
+            }
+
+            if (level.CoreMode == newMode)
                 return;
-            level.CoreMode = mode;
+
+            level.CoreMode = newMode;
+
             if (playEffects) {
                 Input.Rumble(RumbleStrength.Medium, RumbleLength.Medium);
                 level.Flash(Color.White * 0.15f, true);
