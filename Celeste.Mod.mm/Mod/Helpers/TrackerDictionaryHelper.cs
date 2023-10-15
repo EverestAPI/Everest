0d61f003f2c41483854837eba4716ea7a98a83d7
microlith57 <microlith57@gmail.com>
2023-10-16 12:39:13 +1300

    implement type fullname comparer for tracker dictionaries

---
+++ Celeste.Mod.mm/Mod/Helpers/TrackerDictionaryHelper.cs
@@ -0,0 +1,23 @@
+using Monocle;
+using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod {
+    public static class TrackerDictionaryHelper {
+
+        public class TypeNameEqualityComparer : IEqualityComparer<Type> {
+            public bool Equals(Type x, Type y) {
+                return (x == y) || x.FullName.Equals(y.FullName, StringComparison.OrdinalIgnoreCase);
+            }
+
+            public int GetHashCode(Type type) => type.FullName.GetHashCode();
+        }
+
+        public static Dictionary<Type, List<Entity>> MakeEqualityComparerDictionaryForEntityTracker(int capacity)
+            => new Dictionary<Type, List<Entity>>(capacity, new TypeNameEqualityComparer());
+
+        public static Dictionary<Type, List<Component>> MakeEqualityComparerDictionaryForComponentTracker(int capacity)
+            => new Dictionary<Type, List<Component>>(capacity, new TypeNameEqualityComparer());
+
+    }
+}
