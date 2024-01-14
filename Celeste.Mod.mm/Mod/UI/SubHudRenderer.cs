759f28dc4c92df5107302b38415724825cf248a3
Brokemia <37253216+Brokemia@users.noreply.github.com>
2024-01-13 23:35:16 -0500

    Add event for before the subhud render

--- Celeste.Mod.mm/Mod/UI/SubHudRenderer.cs
+++ Celeste.Mod.mm/Mod/UI/SubHudRenderer.cs
@@ -62,6 +62,7 @@ namespace Celeste.Mod.UI {
             if (!scene.Entities.HasVisibleEntities(TagsExt.SubHUD))
                 return;
 
+            Everest.Events.SubHudRenderer.BeforeRender(this, scene);
             BeginRender(null, null);
             scene.Entities.RenderOnly(TagsExt.SubHUD);
             EndRender();
