4155fc57a9511a7124a6a28171a5c66d7e35525b
Maddie <52103563+maddie480@users.noreply.github.com>
2024-03-23 22:14:17 +0100

    Shut down Discord Game SDK whenever there's an error when running the callbacks

--- Celeste.Mod.mm/Mod/Everest/Everest.DiscordSDK.cs
+++ Celeste.Mod.mm/Mod/Everest/Everest.DiscordSDK.cs
@@ -122,12 +122,8 @@ namespace Celeste.Mod {
                 try {
                     DiscordInstance.RunCallbacks();
                 } catch (Discord.ResultException e) {
-                    if (e.Message == nameof(Discord.Result.NotRunning)) {
-                        Logger.Log(LogLevel.Warn, "discord-game-sdk", "Discord was shut down! Disposing Game SDK.");
-                        Dispose();
-                    } else {
-                        throw;
-                    }
+                    Logger.Log(LogLevel.Warn, "discord-game-sdk", $"Failed to run Discord callbacks ({e.Message})! Disposing Game SDK.");
+                    Dispose();
                 }
             }
 
