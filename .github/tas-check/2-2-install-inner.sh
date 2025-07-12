d2bff3c44648dc2dcbd2fe05d3d08b9d825c9e4a
Maddie <52103563+maddie480@users.noreply.github.com>
2025-07-12 23:14:32 +0200

    Update 2-2-install-inner.sh

--- .github/tas-check/2-2-install-inner.sh
+++ .github/tas-check/2-2-install-inner.sh
@@ -27,7 +27,7 @@ rm -v t.zip
 
 # install CelesteTAS
 cd celeste/Mods
-curl --fail -Lo CelesteTAS.zip "https://github.com/EverestAPI/CelesteTAS-EverestInterop/releases/download/v3.45.1/CelesteTAS.zip"
+curl --fail -Lo CelesteTAS.zip "https://github.com/EverestAPI/CelesteTAS-EverestInterop/releases/download/v3.45.2/CelesteTAS.zip"
 
 # install the mod that is going to be TASed, downloaded as a bundle zip containing the mod zip
 # and all of its dependencies (https://maddie480.ovh/celeste/bundle-download?id=${TAS_TO_RUN})
