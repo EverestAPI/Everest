7f5519c1fe9f9e19ee45f8e777fe819b710a5c31
wuke32767 <2446214230@qq.com>
2025-07-31 20:48:15 +0800

    add documentation to SwapImmediatelyExtension

--- Celeste.Mod.mm/Mod/Helpers/SwapImmediatelyExtension.cs
+++ Celeste.Mod.mm/Mod/Helpers/SwapImmediatelyExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Monocle;
+using System;
 using System.Collections;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,15 @@ using System.Collections.Generic;
 
 namespace Celeste.Mod {
     public static class SwapImmediatelyExtension {
+        /// <summary>
+        ///   when you want to enumerate a coroutine ienumerator,
+        ///   always keep in mind that the original sequence may have been wrapped into <see cref="SwapImmediately"/>.
+        ///   this method helps you to handle it correctly like <see cref="Coroutine"/>.<br/>
+        ///   also known as, flattening the given sequence.
+        /// </summary>
+        /// <returns>
+        ///   A new enumerator, but all <see cref="SwapImmediately"/> is safely handled.
+        /// </returns>
         public static IEnumerator SafeEnumerate(this IEnumerator self) {
             Stack<IEnumerator> enums = new();
             enums.Push(self);
