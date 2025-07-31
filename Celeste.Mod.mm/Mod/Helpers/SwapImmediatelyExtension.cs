e6aac5a59056abb0ef5b8c3214c7ecabfbc8d19a
wuke32767 <2446214230@qq.com>
2025-07-31 17:39:24 +0800

    provides a method to safely enumerate SwapImmediately

---
+++ Celeste.Mod.mm/Mod/Helpers/SwapImmediatelyExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Celeste.Mod {
+    public static class SwapImmediatelyExtension {
+        public static IEnumerator SafeEnumerate(this IEnumerator self) {
+            Stack<IEnumerator> enums = new();
+            enums.Push(self);
+
+            while (enums.Count > 0) {
+                IEnumerator cur = enums.Peek();
+
+                if (cur.MoveNext()) {
+                    object obj = cur.Current;
+
+                    if (obj is SwapImmediately swap) {
+                        enums.Push(swap.Inner);
+                    } else {
+                        yield return obj;
+                    }
+                } else {
+                    enums.Pop();
+                }
+            }
+        }
+    }
+}
