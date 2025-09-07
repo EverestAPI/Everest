de4bd46ae8454c8b8b9530d4d9d00064f3475e71
SnipUndercover <snipundercover@gmail.com>
2025-09-08 00:20:31 +0200

    Update `EffectiveTimeRate` documentation

--- Celeste.Mod.mm/Patches/Monocle/Engine.cs
+++ Celeste.Mod.mm/Patches/Monocle/Engine.cs
@@ -132,8 +132,9 @@ namespace Monocle {
         /// <summary>
         /// The final time rate factor applied to <see cref="Engine.RawDeltaTime"/> before being set to
         /// <see cref="Engine.DeltaTime"/>.<br />
-        /// This value is calculated by multiplying <see cref="TimeRate"/>, <see cref="TimeRateB"/>, and the
-        /// <see cref="TimeRateModifier.Multiplier"/>s of all enabled <see cref="TimeRateModifier"/>s in the scene together.
+        /// This value is calculated <b>at the beginning of the frame</b> by multiplying
+        /// <see cref="TimeRate"/>, <see cref="TimeRateB"/>, and the <see cref="TimeRateModifier.Multiplier"/>s
+        /// of all enabled <see cref="TimeRateModifier"/>s in the scene together.
         /// </summary>
         public static float EffectiveTimeRate;
 
