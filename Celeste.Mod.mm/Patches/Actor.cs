a2d191b733ee44c82301930364171ab134e1884e
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-27 14:09:33 -0600

    Created Move methods in Actor to take Vector2 and Point

--- Celeste.Mod.mm/Patches/Actor.cs
+++ Celeste.Mod.mm/Patches/Actor.cs
@@ -1,7 +1,9 @@
 ﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
 
 using Microsoft.Xna.Framework;
+using Monocle;
 using MonoMod;
+using System;
 
 namespace Celeste {
     class patch_Actor : Actor {
@@ -30,5 +32,84 @@ namespace Celeste {
             MoveV((float) ((double) toY - Position.Y - movementCounter.Y), onCollide);
         }
 
+        public void ZeroRemainder() {
+            movementCounter = Vector2.Zero;
+        }
+
+        private static Point VectorToPoint(Vector2 source) {
+            return new(
+                (int) Math.Round(source.X, MidpointRounding.ToEven),
+                (int) Math.Round(source.Y, MidpointRounding.ToEven)
+            );
+        }
+
+        public bool Move(Vector2 move, Collision onCollide = null, Solid pusher = null) {
+            movementCounter += move;
+            Point num = VectorToPoint(move);
+            if (num != Point.Zero) {
+                movementCounter.X -= num.X;
+                movementCounter.Y -= num.Y;
+                return MoveExact(num, onCollide, pusher);
+            }
+            return false;
+        }
+
+        public bool MoveExact(Vector2 move, Collision onCollide = null, Solid pusher = null)
+            => MoveExact(VectorToPoint(move), onCollide, pusher);
+
+        public bool MoveExact(Point move, Collision onCollide = null, Solid pusher = null) {
+            Vector2 targetPosition = Position + Vector2.UnitX * move.X + Vector2.UnitY * move.Y;
+            Point num = new(Math.Sign(move.X), Math.Sign(move.Y));
+            Point num2 = new(0, 0);
+            while (move.X != 0 && move.Y != 0) {
+                Platform platform = CollideFirst<Solid>(Position + Vector2.UnitX * num.X + Vector2.UnitY * num.Y);
+                if (platform != null) {
+                    movementCounter = Vector2.Zero;
+                    onCollide?.Invoke(new CollisionData {
+                        Direction = Vector2.UnitX * num.X + Vector2.UnitY * num.Y,
+                        Moved = Vector2.UnitX * num2.X + Vector2.UnitY * num2.Y,
+                        TargetPosition = targetPosition,
+                        Hit = platform,
+                        Pusher = pusher
+                    });
+                    return true;
+                }
+                if (move.Y > 0 && !IgnoreJumpThrus) {
+                    platform = CollideFirstOutside<JumpThru>(Position + Vector2.UnitX * num.X + Vector2.UnitY * num.Y);
+                    if (platform != null) {
+                        movementCounter = Vector2.Zero;
+                        onCollide?.Invoke(new CollisionData {
+                            Direction = Vector2.UnitX * num.X + Vector2.UnitY * num.Y,
+                            Moved = Vector2.UnitX * num2.X + Vector2.UnitY * num2.Y,
+                            TargetPosition = targetPosition,
+                            Hit = platform,
+                            Pusher = pusher
+                        });
+                        return true;
+                    }
+                }
+                num2.X += num.X;
+                num2.Y += num.Y;
+                move.X -= num.X;
+                move.Y -= num.Y;
+                base.X += num.X;
+                base.Y += num.Y;
+            }
+            return false;
+        }
+
+        public void MoveTowards(Vector2 target, Vector2 maxAmount, Collision onCollide = null) {
+            Vector2 to = patch_Calc.Approach(ExactPosition, target, maxAmount);
+            MoveTo(to, onCollide);
+        }
+
+        public void MoveTowards(Vector2 target, float maxAmount, Collision onCollide = null) {
+            Vector2 to = patch_Calc.Approach(ExactPosition, target, maxAmount);
+            MoveTo(to, onCollide);
+        }
+
+        public void MoveTo(Vector2 to, Collision onCollide = null) {
+            Move(to - Position - movementCounter, onCollide);
+        }
     }
 }
