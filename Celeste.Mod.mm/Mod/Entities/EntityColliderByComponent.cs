1616aef316a50fa91472fc9bee4440dfc0b0f768
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-28 13:40:42 -0600

    Revert "Made EntityCollider classes extend new parent class while using new trackIfNeeded methods from tracker"
    This reverts commit 4c5c009491e38c952d56b2211e6bb56f4dc25a53.

--- Celeste.Mod.mm/Mod/Entities/EntityColliderByComponent.cs
+++ Celeste.Mod.mm/Mod/Entities/EntityColliderByComponent.cs
@@ -1,6 +1,6 @@
 ﻿using Monocle;
 using System;
-using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace Celeste.Mod.Entities {
     /// <summary>
@@ -9,17 +9,64 @@ namespace Celeste.Mod.Entities {
     /// Performs the Action provided on collision. 
     /// </summary>
     /// <typeparam name="T">The specific type of Component this component should try to collide with</typeparam>
-    public class EntityColliderByComponent<T> : CustomCollider<T> where T : Component {
-        public EntityColliderByComponent(Action<T> onEntityAction, Collider collider = null)
-            : base(onEntityAction, collider)
-        {
+    public class EntityColliderByComponent<T> : Component where T : Component {
+        /// <summary>
+        /// The Action invoked on Collision, with the Component collided with passed as a parameter
+        /// </summary>
+        public Action<T> OnComponentAction;
+
+        public Collider Collider;
+
+        public EntityColliderByComponent(Action<T> onComponentAction, Collider collider = null)
+            : base(active: true, visible: true) {
+            OnComponentAction = onComponentAction;
+            Collider = collider;
         }
 
-        protected override Entity GetEntityFromItem(T item) => item.Entity;
-        
+        public override void Added(Entity entity) {
+            base.Added(entity);
+            //Only called if Component is added post Scene Begin and Entity Adding and Awake time.
+            if (Scene != null) {
+                if (!Scene.Tracker.IsComponentTracked<T>()) {
+                    patch_Tracker.AddTypeToTracker(typeof(T));
+                }
+                patch_Tracker.Refresh(Scene);
+            }
+        }
 
-        protected override IEnumerable<T> GetObjectsToCollide() {
-            return (IEnumerable<T>) (Scene.Tracker as patch_Tracker).GetComponentsTrackIfNeeded<T>();
+        public override void EntityAdded(Scene scene) {
+            if (!scene.Tracker.IsComponentTracked<T>()) {
+                patch_Tracker.AddTypeToTracker(typeof(T));
+            }
+            base.EntityAdded(scene);
+        }
+
+        public override void EntityAwake() {
+            patch_Tracker.Refresh(Scene);
+        }
+
+        public override void Update() {
+            if (OnComponentAction == null) {
+                return;
+            }
+
+            Collider collider = Entity.Collider;
+            if (Collider != null) {
+                Entity.Collider = Collider;
+            }
+
+            Entity.CollideDoByComponent(OnComponentAction);
+
+            Entity.Collider = collider;
+        }
+
+        public override void DebugRender(Camera camera) {
+            if (Collider != null) {
+                Collider collider = Entity.Collider;
+                Entity.Collider = Collider;
+                Collider.Render(camera, Color.HotPink);
+                Entity.Collider = collider;
+            }
         }
     }
 }
