d76e286588fd2fee4b2b27cdf7b5a2ca68460a5f
aonkeeper4 <aonkeeper4@gmail.com>
2025-12-18 14:16:57 +0000

    Add `onlyOnce` option to `EventTrigger` (defaulting to `true`), enabling triggering a cutscene multiple times per room load

--- Celeste.Mod.mm/Patches/EventTrigger.cs
+++ Celeste.Mod.mm/Patches/EventTrigger.cs
@@ -18,38 +18,43 @@ namespace Celeste {
         // whether to use a `TalkComponent` to trigger the cutscene instead
         private bool useInteract;
         // whether `OnEnter` was called via the `TalkComponent` callback
-        private bool interactTriggered;
+        private bool triggeredViaInteract;
         // the `TalkComponent` itself
         private TalkComponent talkComponent;
 
-        // a flag which prevents this trigger from loading when enabled
+        // whether this trigger should only activate once per room load
+        private bool onlyOnce;
+        
+        // the flag which prevents this trigger from loading when enabled
         private string deleteFlag;
 
         private patch_EventTrigger(EntityData data, Vector2 offset) : base(data, offset) { }
         
-        // patch the constructor to set `useInteract` and `deleteFlag` and add the `TalkComponent` if necessary
+        // patch the constructor to set `useInteract`, `onlyOnce` and `deleteFlag` and add the `TalkComponent` if necessary
         public extern void orig_ctor(EntityData data, Vector2 offset);
         [MonoModConstructor]
         public void ctor(EntityData data, Vector2 offset) {
             orig_ctor(data, offset);
 
             useInteract = data.Bool("useInteract");
+            onlyOnce = data.Bool("onlyOnce", true);
+            deleteFlag = data.Attr("deleteFlag");
+            
             if (useInteract)
                 Add(talkComponent = new TalkComponent(
                         new Rectangle(0, 0, (int) Width, (int) Height),
-                        (data.FirstNodeNullable(offset) ?? Center) - Position,
+                        (data.FirstNodeNullable(offset) ?? TopCenter) - Position,
                         player => {
-                            interactTriggered = true;
+                            triggeredViaInteract = true;
                             OnEnter(player);
+                            triggeredViaInteract = false;
                         }
                     ) {
                         PlayerMustBeFacing = false
                     });
-
-            deleteFlag = data.Attr("deleteFlag");
         }
 
-        // remove this trigger if the flag with name `deleteFlag` is set
+        // remove this trigger if the flag `deleteFlag` is set
         public override void Added(Scene scene) {
             base.Added(scene);
 
@@ -122,15 +127,16 @@ namespace MonoMod {
         public static void PatchEventTriggerOnEnter(MethodDefinition method, CustomAttribute _) {
             /*
              * we want to:
-             * 1. add a `|| (this.useInteract && !this.interactTriggered)` to the check for `this.triggered`
+             * 1. add a `&& this.onlyOnce || (this.useInteract && !this.triggeredViaInteract)` to the check for `this.triggered`
              * 2. set `this.talkComponent.Enabled = false;` when setting `this.triggered = true;`
              * 3. replace the throw in the default case of the switch statement with a call to `TriggerCustomEvent`
              */
             
             MethodDefinition m_TriggerCustomEvent = method.DeclaringType.FindMethod("System.Void TriggerCustomEvent(Celeste.EventTrigger,Celeste.Player,System.String)")!;
 
+            FieldDefinition f_onlyOnce = method.DeclaringType.FindField("onlyOnce")!;
             FieldDefinition f_useInteract = method.DeclaringType.FindField("useInteract")!;
-            FieldDefinition f_interactTriggered = method.DeclaringType.FindField("interactTriggered")!;
+            FieldDefinition f_triggeredViaInteract = method.DeclaringType.FindField("triggeredViaInteract")!;
             FieldDefinition f_talkComponent = method.DeclaringType.FindField("talkComponent")!;
             FieldDefinition f_Event = method.DeclaringType.FindField("Event")!;
 
@@ -139,7 +145,7 @@ namespace MonoMod {
             new ILContext(method).Invoke(il => {
                 ILCursor cursor = new(il);
                 
-                // 1. add a `|| (this.useInteract && !this.interactTriggered)` to the check for `this.triggered`
+                // 1. add a `&& this.onlyOnce || (this.useInteract && !this.triggeredViaInteract)` to the check for `this.triggered`
                 cursor.GotoNext(MoveType.After, instr => instr.MatchLdfld("Celeste.EventTrigger", "triggered"));
                 
                 // retrieve labels pointing to the `ret` and after it
@@ -151,8 +157,11 @@ namespace MonoMod {
                 // remove the `brfalse.s`
                 cursor.Remove();
                 
-                // emit `this.triggered || (this.useInteract && !this.interactTriggered)`
+                // emit `(this.triggered && this.onlyOnce) || (this.useInteract && !this.triggeredViaInteract)`
                 // `this.triggered` already on the stack
+                cursor.EmitLdarg0();
+                cursor.EmitLdfld(f_onlyOnce); // `this.onlyOnce`
+                cursor.EmitAnd();
                 cursor.EmitBrtrue(ret);
                 
                 cursor.EmitLdarg0();
@@ -160,7 +169,7 @@ namespace MonoMod {
                 cursor.EmitBrfalse(afterRet);
                 
                 cursor.EmitLdarg0();
-                cursor.EmitLdfld(f_interactTriggered); // `this.interactTriggered`
+                cursor.EmitLdfld(f_triggeredViaInteract); // `this.triggeredViaInteract`
                 cursor.EmitBrtrue(afterRet);
                 
                 // 2. set `this.talkComponent.Enabled = false;` when setting `this.triggered = true;`
@@ -169,9 +178,13 @@ namespace MonoMod {
                 // define label to skip to
                 ILLabel afterDisable = cursor.DefineLabel();
 
-                // skip disabling `this.talkComponent` if `this.useInteract` is `false`
+                // skip disabling `this.talkComponent` if `this.useInteract && this.onlyOnce`
                 cursor.EmitLdarg0();
                 cursor.EmitLdfld(f_useInteract); // `this.useInteract`
+                cursor.EmitBrfalse(afterDisable);
+                
+                cursor.EmitLdarg0();
+                cursor.EmitLdfld(f_onlyOnce); // `this.onlyOnce`
                 cursor.EmitBrfalse(afterDisable);
                 
                 // emit `this.talkComponent.Enabled = false;`
@@ -196,7 +209,7 @@ namespace MonoMod {
                 cursor.EmitLdfld(f_Event); // `this.Event`
                 cursor.EmitCall(m_TriggerCustomEvent);
                 
-                // emit a `ret` to end the switch case
+                // emit `ret` to end the switch case
                 cursor.EmitRet();
             });
         }
