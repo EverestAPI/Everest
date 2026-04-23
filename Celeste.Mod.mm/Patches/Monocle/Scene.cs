b87547cc2ad8f5e213957f2241258f88fcfe2785
Maddie <52103563+maddie480@users.noreply.github.com>
2026-04-23 22:25:09 +0200

    Revert "Patch Scene.OnInterval float jank"
    This reverts commit fa7fc64f

--- Celeste.Mod.mm/Patches/Monocle/Scene.cs
+++ Celeste.Mod.mm/Patches/Monocle/Scene.cs
@@ -14,16 +14,6 @@ namespace Monocle {
     	public new event Action OnEndOfFrame;
 #pragma warning restore CS0414
 
-        [MonoModReplace]
-        public new bool OnInterval(float interval) {
-            return (int) (((double) TimeActive - Engine.DeltaTime) / interval) < (int) ((double) TimeActive / interval);
-        }
-
-        [MonoModReplace]
-        public new bool OnInterval(float interval, float offset) {
-            return Math.Floor(((double) TimeActive - offset - Engine.DeltaTime) / interval) < Math.Floor(((double) TimeActive - offset) / interval);
-        }
-
                 
         /// <summary>
         /// Finds all entities created from an EntityData with the specified SID, using the Tracker if possible.
