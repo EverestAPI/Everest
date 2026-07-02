4d6c57fa43102fa44a7e19fd06bec4c3f6344ae1
Daxcr <daxaroodles@gmail.com>
2026-07-02 18:28:09 +1200

    Minor bug fix in MiniInstaller for Windows

--- MiniInstaller/Program.cs
+++ MiniInstaller/Program.cs
@@ -91,7 +91,7 @@ namespace MiniInstaller {
 
                 DepCalls.LoadModders();
 
-                File.Copy(Path.Combine(Globals.PathGame, "FNA.dll"), moddedFNA, overwrite: true);
+                File.Copy(Path.Combine(Globals.PathGame, "everest-lib/FNA.dll"), moddedFNA, overwrite: true);
 
                 // DepCalls.ConvertToNETCore also converts dependencies, so FNA will also be copied to the workspace
                 DepCalls.ConvertToNETCore(Path.Combine(Globals.PathOrig, "Celeste.exe"), moddedCeleste);
